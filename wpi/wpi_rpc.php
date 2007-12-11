83671d4d7627b741739e268fc9e44db4c952c31a
Thomas Kelder <thomaskelder@gmail.com>
2007-12-11 10:45:49 +0000

    updated converter call in xml-rpc interface

--- wpi/wpi_rpc.php
+++ wpi/wpi_rpc.php
@@ -124,7 +124,7 @@ function convertPathway($gpmlData64, $fileType) {
 	$gpmlFile = tempnam(WPI_TMP_PATH, "gpml");
 	writeFile($gpmlFile, $gpmlData);
 	$imgFile = tempnam(WPI_TMP_PATH, $fileType) . ".$fileType";
-	$cmd = "java -jar bin/pathvisio_converter.jar $gpmlFile $imgFile 2>&1";
+	$cmd = "java -jar bin/pathvisio_core.jar $gpmlFile $imgFile 2>&1";
 	wfDebug($cmd);
 	exec($cmd, $output, $status);
 	

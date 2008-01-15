7c3b9b9b220effa5291c48f8637778be2f39a3d9
Alexander Pico <apico@gladstone.ucsf.edu>
2008-01-15 03:56:18 +0000

    Converted Google CSE from "stored" to "linked" to allow more control

--- includes/SpecialSearch.php
+++ includes/SpecialSearch.php
@@ -217,10 +217,10 @@ class SpecialSearch {
 
 		$wgOut->addHTML('
 <!-- Google Search Result Snippet Begins -->
-  <div id="results_011541552088579423722:rset6ep3k64"></div>
+  <div id="results_cref"></div>
   <script type="text/javascript">
-    var googleSearchIframeName = "results_011541552088579423722:rset6ep3k64";
-    var googleSearchFormName = "searchbox_011541552088579423722:rset6ep3k64";
+    var googleSearchIframeName = "results_cref";
+    var googleSearchFormName = "searchbox_cref";
     var googleSearchFrameWidth = 600;
     var googleSearchFrameborder = 0;
     var googleSearchDomain = "www.google.com";
@@ -228,19 +228,6 @@ class SpecialSearch {
   </script>
   <script type="text/javascript" src="http://www.google.com/afsonline/show_afs_search.js"></script>
 <!-- Google Search Result Snippet Ends -->
-          
-<!-- Google Search Result Snippet Begins 
-  <div id="results_002915365922082279465:6qd0wwvwtwu"></div>
-  <script type="text/javascript">
-    var googleSearchIframeName = "results_002915365922082279465:6qd0wwvwtwu";
-    var googleSearchFormName = "searchbox_002915365922082279465:6qd0wwvwtwu";
-    var googleSearchFrameWidth = 600;
-    var googleSearchFrameborder = 0;
-    var googleSearchDomain = "www.google.com";
-    var googleSearchPath = "/cse";
-  </script>
-  <script type="text/javascript" src="http://www.google.com/afsonline/show_afs_search.js"></script>
-Google Search Result Snippet Ends -->
 		');
 	}
 

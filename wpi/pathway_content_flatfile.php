837a8174cdeadfff8750a97968e99b67df8fc8cd
Thomas Kelder <thomaskelder@gmail.com>
2008-02-08 12:57:30 +0000

    flatfile: - removed hard-coded species restriction, now specify via request - added latest revision number - removed <?xml tag at end of flatfile

--- wpi/pathway_content_flatfile.php
+++ wpi/pathway_content_flatfile.php
@@ -3,15 +3,20 @@ require_once("wpi.php");
 require_once("Pathway.php");
 require_once("PathwayData.php");
 
-print "Pathway Name\tOrganism\tCategories\tUrl to WikiPathways\tLast Changed\tAuthor\tCount\tDatanodes\n";
+//Get species restriction
+$restrictSpecies = $_REQUEST['species'];
+
+print "Pathway Name\tOrganism\tCategories\tUrl to WikiPathways\tLast Changed\tLast revision\tAuthor\tCount\tDatanodes\n";
 
 $all_pathways = Pathway::getAllPathways();
 foreach (array_keys($all_pathways) as $pathway) {
 
 $species = $all_pathways[$pathway]->species();
 
-//Uncomment to restrict by species
-if ($species != "Homo sapiens") continue; 	
+//Apply species restriction if necessary
+if($restrictSpecies) {
+	if ($species != $restrictSpecies) continue; 	
+}
 
 $xml = $all_pathways[$pathway]->getPathwayData();  
 $gpml = $xml->getGpml();
@@ -20,13 +25,14 @@ $modTime = $all_pathways[$pathway]->getGpmlModificationTime();
 $url = $all_pathways[$pathway]->getFullUrl();
 $author = $gpml["Author"];
 $categories = "";
+$lastRevision = $all_pathways[$pathway]->getLatestRevision();
 $catArray = $xml->getWikiCategories();
 foreach ($catArray as $cat){
 	$categories .= $cat.",";
 }
 perlChop($categories);
 
-print $name."\t".$species."\t".$categories."\t".$url."\t".$modTime."\t".$author."\t";
+print $name."\t".$species."\t".$categories."\t".$url."\t".$modTime."\t".$lastRevision."\t".$author."\t";
 
 $nodes = $xml->getUniqueElements('DataNode', 'TextLabel');
 //print count($nodes)."\t";
@@ -47,10 +53,6 @@ perlChop($geneList);
 print $geneList."\n";
 
 }
-
-$dom = new DOMDocument('1.0');
-$dom->formatOutput = true;
-echo $dom->saveXML();
 
 function perlChop(&$string){
         $endchar = substr("$string", strlen("$string") - 1, 1);

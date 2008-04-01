fb462cd45246cee1f80acc0a68c4c82e0220b649
Thomas Kelder <thomaskelder@gmail.com>
2008-04-01 19:41:57 +0000

    fixed #400

--- wpi/extensions/movePathway.php
+++ wpi/extensions/movePathway.php
@@ -1,6 +1,21 @@
 <?php
 require_once('wpi/wpi.php');
-$wgHooks['SpecialMovepageAfterMove'][] = 'movePathwayPages';
+
+$wgHooks['AbortMove'][] = 'checkMoveAllowed';$wgHooks['SpecialMovepageAfterMove'][] = 'movePathwayPages';
+
+/**
+ * Handles actions before move: check if the pathway name is valid and
+ * return error message if not
+ */
+function checkMoveAllowed($oldtitle, $newtitle, $usser, &$error) {
+	try {
+		$pwNew = Pathway::newFromTitle($newtitle);
+	} catch(Exception $e) {
+		$error = $e->getMessage();
+		return false;
+	}
+	return true;
+}
 
 /**
  * Handles actions needed after moving a page in the pathway namespace
@@ -10,7 +25,7 @@ function movePathwayPages(&$movePageForm , &$ot , &$nt) {
 		$pwOld = Pathway::newFromTitle($ot);		
 		//Clean up old cache and update for the new page
 		$pwOld->clearCache(null, true);
-		
+	
 		$pwNew = Pathway::newFromTitle($nt);
 		$pwNew->updateCache();
 		$pwNew->updateCategories();

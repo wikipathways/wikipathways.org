8a8b5f35324782260a8a4ba0621ddff83292eb70
Thomas Kelder <thomaskelder@gmail.com>
2008-06-23 10:26:35 +0000

    fixed #553

--- wpi/extensions/CheckGpmlOnSave.php
+++ wpi/extensions/CheckGpmlOnSave.php
@@ -4,12 +4,22 @@ require_once('wpi/wpi.php');
 //Register a hook that checks for valid GPML for all
 //saves in the Pathway namespace
 $wgHooks['ArticleSave'][] = 'wfCheckGpml';
+//Register a hook that updates categories after saving
+$wgHooks['ArticleSaveComplete'][] = 'wfUpdateAfterSave';
 
 function wfCheckGpml(&$article, &$user, &$text, &$summary, $minor, $watch, $sectionanchor, &$flags) {
 	if($article->getTitle()->getNamespace() == NS_PATHWAY) {
 		if($error = Pathway::validateGpml($text)) {
 			return "<h1>Invalid GPML</h1><p><code>$error</code>";
 		}
+	}
+	return true;
+}
+
+function wfUpdateAfterSave(&$article, &$user, &$text, &$summary, $minor, $watch, $sectionanchor, &$flags) {
+	if($article->getTitle()->getNamespace() == NS_PATHWAY) {
+		$pw = Pathway::newFromTitle($article->getTitle());
+		$pw->updateCategories();
 	}
 	return true;
 }

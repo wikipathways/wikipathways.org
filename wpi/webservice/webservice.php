27d125e47f63b103fc8139070533ac41f5b88fd9
Thomas Kelder <thomaskelder@gmail.com>
2008-07-28 16:07:04 +0000

    added soap webservice using WSO2

---
+++ wpi/webservice/webservice.php
@@ -0,0 +1,284 @@
+<?php
+$dir = getcwd();
+chdir("../");
+require_once('wpi.php');
+chdir($dir);
+
+$operations = array(
+	"getPathwayList", 
+	"getPathway",
+	"getRecentChanges",
+	"login",
+	"getPathwayAs",
+	"updatePathway",
+);
+$opParams = array(
+	"getPathwayList" => "MIXED", 
+	"getPathway" => "MIXED",
+	"getRecentChanges" => "MIXED",
+	"login" => "MIXED",
+	"getPathwayAs" => "MIXED",
+	"updatePathway" => "MIXED",
+);
+
+$classmap = array(); //just let the engine know you prefer classmap mode
+
+$svr = new WSService(array(
+	"operations" => $operations,
+	"classmap" => $classmap,
+	"opParams" => $opParams,
+	"serviceName" => "WikiPathways",
+));
+
+$svr->reply();
+
+/**
+ * Get a list of all available pathways.
+ * @return array of object WSPathwayInfo $pathways Array of pathway info objects
+ **/
+function getPathwayList() {
+	$pathways = Pathway::getAllPathways();
+	$objects = array();
+	foreach($pathways as $p) {
+		$objects[] = new WSPathwayInfo($p);
+	}
+	return array("pathways" => $objects);
+}
+
+/**
+ * Get the GPML code for a pathway
+ * @param string $pwName The pathway name
+ * @param string $pwSpecies The pathway species
+ * @param integer $revision The revision number of the pathway (use 0 for most recent)
+ * @return object WSPathway $pathway The pathway
+ **/
+function getPathway($pwName, $pwSpecies, $revision = 0) {
+	try {
+		$pathway = new Pathway($pwName, $pwSpecies);
+		$pwi = new WSPathway($pathway);
+		return array("pathway" => $pwi);
+	} catch(Exception $e) {
+		wfDebug("ERROR: $e");
+		throw new WSFault("Receiver", $e);
+	}
+}
+
+/**
+ * Update the GPML code of a pathway on the wiki
+ * @param string $pwName The name of the pathway
+ * @param string $pwSpecies The species of the pathway
+ * @param string $description A description of the modifications
+ * @param string $gpml The updated GPML code
+ * @param int $revision The revision the GPML code is based on
+ * @param object WSAuth $auth The authentication info
+ * @return boolean $success Whether the update was successful
+ **/
+function updatePathway($pwName, $pwSpecies, $description, $gpml, $revision, $auth = NULL) {
+	global $wgUser;
+	
+	try {
+		//Authenticate first, if token is provided
+		if($auth) {
+			authenticate($auth['user'], $auth['key']);
+		}
+
+		$pathway = new Pathway($pwName, $pwSpecies);
+		//Only update if the given revision is the newest
+		//Or if this is a new pathway
+		if(!$pathway->exists() || $revision == $pathway->getLatestRevision()) {
+			$pathway->updatePathway($gpml, $description);
+			$resp = $pathway->getLatestRevision();
+		} else {
+			throw new WSFault("Sender",
+				"Revision out of date: your GPML code originates from " .
+				"an old revision. This means somebody else modified the pathway " .
+				"since you downloaded it. Please apply your changes on the newest version"
+			);
+		}
+	} catch(Exception $e) {
+		if($e instanceof WSFault) { 
+			throw $e; 
+		} else {
+			throw new WSFault("Receiver", $e);
+			wfDebug("ERROR: $e");
+		}
+	}
+	ob_clean();
+	return array("success" => true);
+}
+
+/**
+ * Start a logged in session, using an existing WikiPathways account. 
+ * This function will return an authentication code that can be used 
+ * to excecute methods that need authentication (e.g. updatePathway)
+ * @param string $name The username
+ * @param string $pass The password
+ * @return string $auth The authentication code
+ **/
+function login($name, $pass) {
+	global $wgUser, $wgAuth;
+	
+	$user = User::newFromName( $name );
+	if( is_null($user) || $user->getID() == 0) {
+		//throw new Exception("Invalid user name");
+		throw new WSFault("Sender", "Invalid user name");
+	}
+	$user->load();
+	if ($user->checkPassword( $pass )) {
+		$wgAuth->updateUser($user);
+		$wgUser = $user;
+		return array("auth" => $user->mToken);
+	} else {
+		//throw new Exception("Wrong password");
+		throw new WSFault("Sender", "Wrong password");
+	}
+}
+
+/**
+ * Download a pathway in the specified file format.
+ * @param string $fileType The file type to convert to, e.g.
+ * 'svg', 'png' or 'txt'
+ * @param string $pwName The pathway name
+ * @param string $pwSpecies The pathway species
+ * @param integer $revision The revision number of the pathway (use 0 for most recent)
+ * @return base64Binary $data The converted file data (base64 encoded)
+ **/
+function getPathwayAs($fileType, $pwName, $pwSpecies, $revision = 0) {
+	try {
+		$p = new Pathway($pwName, $pwSpecies);
+		$p->setActiveRevision($revision);
+		$data = file_get_contents($p->getFileLocation($fileType));
+		$data = base64_encode($data);
+	} catch(Exception $e) {
+		throw new WSFault("Receiver", "Unable to get pathway: " . $e);
+	}
+	return array("data" => $data);
+}
+
+/**
+ * Get the recently changed pathways. Note: the recent changes table
+ * only retains items for a limited time, so it's not guaranteed
+ * that you will get all changes since the given timestamp.
+ * @param string $timestamp Get the changes after this time
+ * @return array of object WSPathwayInfo $pathways A list of the changed pathways
+ **/
+function getRecentChanges($timestamp)
+{
+	//check safety of $timestamp, must be exactly 14 digits and nothing else.
+	if (!preg_match ("/^\d{14}$/", $timestamp))
+	{
+		throw new WSFault("Sender", "Invalid timestamp " . htmlentities ($timestamp));
+	}
+
+	$dbr =& wfGetDB( DB_SLAVE );
+	$forceclause = $dbr->useIndexClause("rc_timestamp");
+	$recentchanges = $dbr->tableName( 'recentchanges');
+
+	$sql = "SELECT  
+				rc_namespace, 
+				rc_title, 
+				MAX(rc_timestamp)
+			FROM $recentchanges $forceclause
+			WHERE 
+				rc_namespace = " . NS_PATHWAY . "
+				AND
+				rc_timestamp > '$timestamp'
+			GROUP BY rc_title
+			ORDER BY rc_timestamp DESC
+		";
+		
+	//~ wfDebug ("SQL: $sql");
+
+	$res = $dbr->query( $sql, "getRecentChanges" );
+
+	$objects = array();
+	while ($row = $dbr->fetchRow ($res))
+	{
+		try {
+				$ts = $row['rc_title'];
+			$p = Pathway::newFromTitle($ts);
+			$objects[] = new WSPathwayInfo($p);
+		} catch(Exception $e) {
+			wfDebug("Unable to create pathway object for recent changes: $e");
+		}
+
+	}
+	return array("pathways" => $objects);
+}
+
+//Non ws functions
+function authenticate($username, $token) {
+	global $wgUser, $wgAuth;
+	$user = User::newFromName( $username );
+	if( is_null($user) || $user->getID() == 0) {
+		throw new WSFault("Sender", "Invalid user name");
+	}
+	$user->load();
+	if ($user->mToken == $token) {
+		$wgAuth->updateUser($user);
+		$wgUser = $user;
+	} else {
+		throw new WSFault("Sender", "Wrong authentication token");
+	}
+}
+
+//Class definitions
+ /**
+ * @namespace http://www.wikipathways.org/webservice
+ */
+class WSPathwayInfo {
+	function __construct($pathway) {
+		$this->revision = $pathway->getLatestRevision();
+		$this->species = $pathway->species();
+		$this->name = $pathway->name();
+		$this->url = $pathway->getTitleObject()->getFullURL();
+	}
+	
+	/**
+	* @property string $url - the url to the pathway
+	**/
+	public $url;
+	/**
+	* @property string $name - the pathway name
+	**/
+	public $name;
+	/**
+	* @property string $species - the pathway species
+	**/
+	public $species;
+	/**
+	* @property string $revision - the revision number
+	**/
+	public $revision;
+}
+
+/**
+ * @namespace http://www.wikipathways.org/webservice
+ */
+class WSPathway extends WSPathwayInfo {
+	function __construct($pathway) {
+		parent::__construct($pathway);
+		$this->gpml = $pathway->getGPML();
+	}
+	/**
+	* @property string $gpml - the GPML code
+	**/
+	public $gpml;
+}
+
+/**
+ * @namespace http://www.wikipathways.org/webservice
+ **/
+class WSAuth {
+	/**
+	 * @property string $user The username
+	 **/
+	public $user;
+	
+	/**
+	 * @property string $key The authentication key
+	 **/
+	public $key;
+}
+
+?>

486995d7977568f37de7fca17e4d2427327d3aee
Thomas Kelder <thomaskelder@gmail.com>
2008-09-24 12:27:36 +0000

    implemented email notification for curation tags

--- wpi/extensions/CurationTags/CurationTags.php
+++ wpi/extensions/CurationTags/CurationTags.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once(WPI_SCRIPT_PATH . "/MetaTag.php");
+require_once("CurationTagsMailer.php");
 
 $wfCurationTagsPath = WPI_URL . "/extensions/CurationTags";
 
@@ -15,8 +16,35 @@ $wgAjaxExportList[] = "CurationTagsAjax::getTagHistory";
 $wgExtensionFunctions[] = "wfCurationTags";
 
 function wfCurationTags() {
-    global $wgParser;
-    $wgParser->setHook( "curationTags", "displayCurationTags" );
+	global $wgParser;
+	$wgParser->setHook( "curationTags", "displayCurationTags" );
+	
+	global $wgMessageCache;
+	$wgMessageCache->addMessages( 
+	array(
+	'tagemail_subject' => '{{SITENAME}} page $PAGETITLE has been changed by $PAGEEDITOR',
+	'tagemail_body' => 'Dear $WATCHINGUSERNAME,
+
+
+$PAGEEDITOR $ACTIONd curation tag "$TAGNAME" on page $PAGETITLE. See $PAGETITLE_URL for the current version.
+
+Contact the editor:
+mail: $PAGEEDITOR_EMAIL
+wiki: $PAGEEDITOR_WIKI
+
+There will be no other notifications in case of further changes unless you visit this page.
+You could also reset the notification flags for all your watched pages on your watchlist.
+
+             Your friendly {{SITENAME}} notification system
+
+--
+To change your watchlist settings, visit
+{{fullurl:{{ns:special}}:Watchlist/edit}}
+
+Feedback and further assistance:
+{{fullurl:{{MediaWiki:Helppage}}}}'
+	)
+);
 }
 
 function displayCurationTags($input, $argv, &$parser) {
@@ -50,6 +78,43 @@ function displayCurationTags($input, $argv, &$parser) {
 	$pageId = $parser->mTitle->getArticleID();
 	$elementId = 'curationTagDiv';
 	return "<div id='$elementId'></div><script type=\"{$wgJsMimeType}\">CurationTags.insertDiv('$elementId', '$pageId');</script>\n";
+}
+
+/**
+ * Processes events after a curation tag has changed
+ */
+function curationTagChanged($tag) {
+	global $wgEnotifUseJobQ;
+	
+	$hist = MetaTag::getHistoryForPage($tag->getPageId(), wfTimestamp(TS_MW));
+
+	if(count($hist) > 0) {
+		$taghist = $hist[0];
+		$enotif = new TagChangeNotification($taghist);
+		$enotif->notifyOnTagChange();
+	}
+}
+
+class CurationTag {
+	private static $tagDefinition;
+	
+	/**
+	 * Get the display name for the given tag
+	 */
+	public static function getDisplayName($tagname) {
+		$xpath = 'Tag[@name="' . $tagname . '"]/@displayName';
+		$dn = self::getTagDefinition()->xpath($xpath);
+		return $dn ? (string)$dn[0]['displayName'] : $tagname;
+	}
+	
+	private static function getTagDefinition() {
+		if(!self::$tagDefinition) {
+			$title = Title::newFromText(CurationTagsAjax::$TAG_LIST_PAGE);
+			$ref = Revision::newFromTitle($title);
+			self::$tagDefinition = new SimpleXMLElement($ref->getText());
+		}
+		return self::$tagDefinition;
+	}
 }
 
 class CurationTagsAjax {
@@ -115,6 +180,8 @@ class CurationTagsAjax {
 		$root->appendChild($doc->createTextNode($name));
 		$doc->appendChild($root);
 		
+		curationTagChanged($tag);
+		
 		$resp = new AjaxResponse(trim($doc->saveXML()));
 		$resp->setContentType("text/xml");
 		return $resp;
@@ -137,6 +204,8 @@ class CurationTagsAjax {
 		$root = $doc->createElement("Name");
 		$root->appendChild($doc->createTextNode($name));
 		$doc->appendChild($root);
+		
+		curationTagChanged($tag);
 		
 		$resp = new AjaxResponse(trim($doc->saveXML()));
 		$resp->setContentType("text/xml");
---
+++ wpi/extensions/CurationTags/CurationTagsMailer.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * Modification to the default EmailNotification class
+ * that sets a custom email text.
+ */
+class TagChangeNotification extends EmailNotification {
+	var $taghist;
+	
+	function __construct($taghist) {
+		$this->taghist = $taghist;
+	}
+	
+		/**
+	 * Send emails corresponding to the user $editor editing the page $title.
+	 * Also updates wl_notificationtimestamp.
+	 *
+	 * May be deferred via the job queue.
+	 *
+	 * @param $editor User object
+	 * @param $title Title object
+	 * @param $timestamp
+	 * @param $summary
+	 * @param $minorEdit
+	 * @param $oldid (default: false)
+	 */
+	function notifyOnTagChange() {
+		$taghist = $this->taghist;
+		$this->actuallyNotifyOnPageChange(
+			User::newFromId($taghist->getUser()),
+			Title::newFromId($taghist->getPageId()),
+			$taghist->getTime(),
+			$taghist->getAction(),
+			"1",
+			false
+		);
+	}
+	
+	function composeCommonMailtext() {
+		global $wgPasswordSender, $wgNoReplyAddress;
+		global $wgEnotifFromEditor, $wgEnotifRevealEditorAddress;
+		global $wgEnotifImpersonal;
+		$this->composed_common = true;
+
+		$subject = $this->template_subject;
+		$body    = $this->template_body;
+		
+		$subject = wfMsgForContent( 'tagemail_subject' );
+		$body    = wfMsgForContent( 'tagemail_body' );
+		
+		$from    = ''; /* fail safe */
+		$replyto = ''; /* fail safe */
+		$keys    = array();
+
+		$pagetitle = $this->title->getPrefixedText();
+		$keys['$PAGETITLE']          = $pagetitle;
+		$keys['$PAGETITLE_URL']      = $this->title->getFullUrl();
+
+		$keys['$ACTION'] = $this->taghist->getAction();
+		$keys['$TAGNAME'] = CurationTag::getDisplayName($this->taghist->getTagName());
+		
+		$subject = strtr( $subject, $keys );
+
+		# Reveal the page editor's address as REPLY-TO address only if
+		# the user has not opted-out and the option is enabled at the
+		# global configuration level.
+		$editor = $this->editor;
+		$name    = $editor->getName();
+		$adminAddress = new MailAddress( $wgPasswordSender, 'WikiAdmin' );
+		$editorAddress = new MailAddress( $editor );
+		if( $wgEnotifRevealEditorAddress
+		    && ( $editor->getEmail() != '' )
+		    && $editor->getOption( 'enotifrevealaddr' ) ) {
+			if( $wgEnotifFromEditor ) {
+				$from    = $editorAddress;
+			} else {
+				$from    = $adminAddress;
+				$replyto = $editorAddress;
+			}
+		} else {
+			$from    = $adminAddress;
+			$replyto = new MailAddress( $wgNoReplyAddress );
+		}
+
+		if( $editor->isIP( $name ) ) {
+			#real anon (user:xxx.xxx.xxx.xxx)
+			$utext = wfMsgForContent('enotif_anon_editor', $name);
+			$subject = str_replace('$PAGEEDITOR', $utext, $subject);
+			$keys['$PAGEEDITOR']       = $utext;
+			$keys['$PAGEEDITOR_EMAIL'] = wfMsgForContent( 'noemailtitle' );
+		} else {
+			$subject = str_replace('$PAGEEDITOR', $name, $subject);
+			$keys['$PAGEEDITOR']          = $name;
+			$emailPage = SpecialPage::getSafeTitleFor( 'Emailuser', $name );
+			$keys['$PAGEEDITOR_EMAIL'] = $emailPage->getFullUrl();
+		}
+		$userPage = $editor->getUserPage();
+		$keys['$PAGEEDITOR_WIKI'] = $userPage->getFullUrl();
+		$body = strtr( $body, $keys );
+		$body = wordwrap( $body, 72 );
+		
+		# now save this as the constant user-independent part of the message
+		$this->from    = $from;
+		$this->replyto = $replyto;
+		$this->subject = $subject;
+		$this->body    = $body;
+		wfDebug(var_export($this->subject, TRUE) . " SUBJ_END\n");
+		wfDebug(var_export($this->body, TRUE) . " BODY_END\n");
+	}
+}
+
+?>

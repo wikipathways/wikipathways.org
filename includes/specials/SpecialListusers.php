fb3a3be2bd4f0716370c980d2d242ccd420d7f11
Alexander Pico <apico@gladstone.ucsf.edu>
2008-10-07 09:46:11 +0000

    replaced group info with links to User_snoop per user

--- includes/specials/SpecialListusers.php
+++ includes/specials/SpecialListusers.php
@@ -101,7 +101,11 @@ class UsersPager extends AlphabeticPager {
 			$groups = '';
 		}
 
-		$item = wfSpecialList( $name, $groups );
+		//AP20081006 - replaced group info with links to User_snoop
+		$snoop = Title::makeTitle( NS_SPECIAL, 'User_snoop');
+		$snooplink = $this->getSkin()->makeKnownLinkObj( $snoop, 'info', wfArrayToCGI( array('username' => $row->user_name)), '','','');
+		
+		$item = wfSpecialList( $name, $snooplink);
 		wfRunHooks( 'SpecialListusersFormatRow', array( &$item, $row ) );
 		return "<li>{$item}</li>";
 	}

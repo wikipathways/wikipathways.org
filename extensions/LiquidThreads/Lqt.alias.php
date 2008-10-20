f725887ec30c4668c179fd837e0480f0d91d5d09
Alexander Pico <apico@gladstone.ucsf.edu>
2008-10-20 20:43:43 +0000

    installed LiquidThread extension for discussion pages

---
+++ extensions/LiquidThreads/Lqt.alias.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Aliases for special pages
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$aliases = array();
+
+/** English */
+$aliases['en'] = array(
+	'DeleteThread' => array( 'DeleteThread' ),
+	'MoveThread' => array( 'MoveThread' ),
+	'NewMessages' => array( 'NewMessages' ),
+);
+
+/** Arabic (العربية)
+ * @author Meno25
+ */
+$aliases['ar'] = array(
+	'DeleteThread' => array( 'حذف_مجموعة_الرسائل' ),
+	'MoveThread' => array( 'نقل_مجموعة_الرسائل' ),
+	'NewMessages' => array( 'رسائل_جديدة' ),
+);
+
+/** Egyptian Spoken Arabic (مصرى)
+ * @author Meno25
+ */
+$aliases['arz'] = array(
+	'DeleteThread' => array( 'حذف_مجموعة_الرسائل' ),
+	'MoveThread' => array( 'نقل_مجموعة_الرسائل' ),
+	'NewMessages' => array( 'رسائل_جديدة' ),
+);
+
+/** Luxembourgish (Lëtzebuergesch) */
+$aliases['lb'] = array(
+	'NewMessages' => array( 'Nei Messagen' ),
+);
+
+/** Nedersaksisch (Nedersaksisch) */
+$aliases['nds-nl'] = array(
+	'DeleteThread' => array( 'Draod_vortdoon' ),
+	'MoveThread' => array( 'Draod_verplaosen' ),
+	'NewMessages' => array( 'Nieje_berichen' ),
+);
+
+/** Dutch (Nederlands) */
+$aliases['nl'] = array(
+	'DeleteThread' => array( 'ThreadVerwijderen' ),
+	'MoveThread' => array( 'ThreadVerplaatsen' ),
+	'NewMessages' => array( 'NieuweBerichten' ),
+);
+
+/** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬) */
+$aliases['no'] = array(
+	'DeleteThread' => array( 'Slett tråd' ),
+	'MoveThread' => array( 'Flytt tråd' ),
+	'NewMessages' => array( 'Nye beskjeder' ),
+);
+
+/** Swedish (Svenska) */
+$aliases['sv'] = array(
+	'DeleteThread' => array( 'Radera tråd' ),
+	'MoveThread' => array( 'Flytta tråd' ),
+	'NewMessages' => array( 'Nya meddelanden' ),
+);
+

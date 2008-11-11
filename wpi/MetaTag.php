38e5d570bcaf59c1b39dca3af810e8d759efd9ef
Alexander Pico <apico@gladstone.ucsf.edu>
2008-11-11 23:18:56 +0000

    removed cache update

--- wpi/MetaTag.php
+++ wpi/MetaTag.php
@@ -216,7 +216,6 @@ class MetaTag {
 		
 		$dbw->immediateCommit();
 		$this->exists = false;
-		StatisticsCache::updatePathwaysCache();
 	}
 	
 	private function doWriteToDB() {
@@ -258,7 +257,6 @@ class MetaTag {
 			
 			$this->writeHistory(self::$ACTION_CREATE);
 		}
- 		StatisticsCache::updatePathwaysCache();
 
 
 	}

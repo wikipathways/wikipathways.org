c186228e46a3399e7efae4ca06f3759780bb0edf
Alexander Pico <apico@gladstone.ucsf.edu>
2009-03-05 00:36:05 +0000

    added link to Private Pathway help page

--- wpi/extensions/CreatePathwayPage/CreatePathwayPage.i18n.php
+++ wpi/extensions/CreatePathwayPage/CreatePathwayPage.i18n.php
@@ -1,8 +1,11 @@
 <?php
+global $wgUser;
+$skin = $wgUser->getSkin();
+$link = $skin->makeLinkObj(Title::makeTitle( NS_HELP, Private_Pathways ), ' (how does that work?)');
 $allMessages = array(
         'en' => array( 
                 'createpathwaypage' => 'Create new pathway',
-                'create_private' => 'Make pathway private',
+                'create_private' => "Make pathway private  $link",
         )
 );
 ?>

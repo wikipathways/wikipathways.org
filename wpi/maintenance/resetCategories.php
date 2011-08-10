39f407c01c3e33220c00bf719300a102344c9f2f
martijn <martijn@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2011-08-10 09:02:47 +0000

    Added some extra log statements to resetCategories maintenance script

--- wpi/maintenance/resetCategories.php
+++ wpi/maintenance/resetCategories.php
@@ -5,7 +5,12 @@ require_once("Maintenance.php");
 ## Reset the MediaWiki categorylinks records for each pathway to contain only the GPML attributes
 ## And remove categories on images
 
+# ini_set('memory_limit', '512M');
+
 $pathways = Pathway::getAllPathways();
+
+
+echo "Number of pathways: " . count($pathways) . "<BR>\n";
 
 foreach($pathways as $pathway) {
 	try {
@@ -20,4 +25,6 @@ foreach($pathways as $pathway) {
 		continue;
 	}
 }
+
+echo "DONE.<BR>\n";
 ?>

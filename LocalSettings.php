bc12597f3d2a481c7b6aaea2927a027b7dea33d3
Thomas Kelder <thomaskelder@gmail.com>
2011-09-30 08:03:54 +0000

    updated ontology version

--- LocalSettings.php
+++ LocalSettings.php
@@ -188,7 +188,7 @@ $wgAllowExternalImages = true;
 
 # Ontologies in JSON format for use in the Javascript
 # Format : ["<Ontology Name>", <Ontology Id>, <Version Id>]
-$wgOntologiesJSON = '[' . '["Pathway Ontology","PW:0000001",1035,42912]' . ',' . '["Disease","DOID:4",1009,44546]' . ',' . '["Cell Type","CL:0000000",1006,44324]]';
+$wgOntologiesJSON = '[' . '["Pathway Ontology","PW:0000001",1035,46237]' . ',' . '["Disease","DOID:4",1009,46309]' . ',' . '["Cell Type","CL:0000000",1006,46163]]';
 # Ontologies Array to be used in the PHP Code
 $wgOntologiesArray = json_decode($wgOntologiesJSON);
 # Email address for the User Identification parameter to be used while making REST calls to BioPortal

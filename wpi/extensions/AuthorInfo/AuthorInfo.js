9309be32818b78af04ad9d1aec2b4204ccdf85f3
martijn <martijn@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2012-09-10 22:53:23 +0000

    Revert "restructured AuthorInfo extension to work with MW1.18"
    This reverts commit 6b84f20e658d03109ba4a47fa354d0c5a8877d8e.

--- wpi/extensions/AuthorInfo/AuthorInfo.js
+++ wpi/extensions/AuthorInfo/AuthorInfo.js
@@ -1,53 +1,53 @@
-AuthorInfo = {
-	/**
-	 * Create an author list for the given page and add it to the
-	 * document in the given div element
-	 */
-	init: function(toDiv, pageId, limit, showBots) {
-		AuthorInfo.pageId = pageId;
-		AuthorInfo.showBots = showBots;
-	
-		parentElm = document.getElementById(toDiv);
-	
-		//The top container
-		var contentDiv = document.createElement("div");
-		contentDiv.id = "AuthorInfo_" + pageId;
-		AuthorInfo.contentDiv = contentDiv;
-		parentElm.appendChild(contentDiv);
-	
-		var authorDiv = document.createElement("div");
-		AuthorInfo.contentDiv.appendChild(authorDiv);
-		AuthorInfo.authorDiv = authorDiv;
-	
-		//Overlay div to show errors
-		AuthorInfo.errorDiv = document.createElement("div");
-		AuthorInfo.errorDiv.className = "authorerror";
-		AuthorInfo.contentDiv.appendChild(AuthorInfo.errorDiv);
-	
-		AuthorInfo.loadAuthors(limit);
-	},
 
-	loadAuthors: function(limit) {
-		AuthorInfo.lastLimit = limit;
-		if(limit == 0) limit = -1;
-		$.get(
-			mw.util.wikiScript(), {
-				action: 'ajax',
-				rs: 'AuthorInfoExtension::jsGetAuthors',
-				rsargs: [AuthorInfo.pageId, parseInt(limit) + 1, false]
-			},
-			AuthorInfo.loadAuthorsCallback
-		)
-		.success(AuthorInfo.loadAuthorsCallback)
-		.error(AuthorInfo.showError);
-	},
+var AuthorInfo = {};
 
-	loadAuthorsCallback: function(xml) {
+/**
+ * Create an author list for the given page and add it to the
+ * document in the given div element
+ */
+AuthorInfo.init = function(toDiv, pageId, limit, showBots) {
+	AuthorInfo.pageId = pageId;
+	AuthorInfo.showBots = showBots;
+	
+	parentElm = document.getElementById(toDiv);
+	
+	//The top container
+	var contentDiv = document.createElement("div");
+	contentDiv.id = "AuthorInfo_" + pageId;
+	AuthorInfo.contentDiv = contentDiv;
+	parentElm.appendChild(contentDiv);
+	
+	var authorDiv = document.createElement("div");
+	AuthorInfo.contentDiv.appendChild(authorDiv);
+	AuthorInfo.authorDiv = authorDiv;
+	
+	//Overlay div to show errors
+	AuthorInfo.errorDiv = document.createElement("div");
+	AuthorInfo.errorDiv.className = "authorerror";
+	AuthorInfo.contentDiv.appendChild(AuthorInfo.errorDiv);
+	
+	AuthorInfo.loadAuthors(limit);
+}
+
+AuthorInfo.loadAuthors = function(limit) {
+	AuthorInfo.lastLimit = limit;
+	if(limit == 0) limit = -1;
+	
+	sajax_do_call(
+		"jsGetAuthors",
+		[AuthorInfo.pageId, parseInt(limit) + 1, false],
+		AuthorInfo.loadAuthorsCallback
+	);
+}
+
+AuthorInfo.loadAuthorsCallback = function(xhr) {
+	if(AuthorInfo.checkResponse(xhr)) {
+		var xml = xhr.responseXML;
 		var elements = xml.getElementsByTagName("Author");
-	
+		
 		var showAll = AuthorInfo.lastLimit <= 0 ||
 			elements.length <= AuthorInfo.lastLimit;
-		
+			
 		var html = "<span class='author'>";
 		var end = showAll ? elements.length : elements.length - 1;
 		for(i=0;i<end;i++) {
@@ -64,20 +64,32 @@ AuthorInfo = {
 				"title='Click to show all authors'>et al.</a>";
 		}
 		AuthorInfo.authorDiv.innerHTML = html + "</span>";
-	},
-
-	showAllAuthors: function() {
-		AuthorInfo.loadAuthors(0);
-	},
-
-	showError: function(msg) {
-		AuthorInfo.errorDiv.style.display = "block";
-		AuthorInfo.errorDiv.innerHTML = "<p class='authorerror'>Error loading authors: " + msg + 
-			" - <a href='javascript:AuthorInfo.hideError();'>close</a></p>";
-	},
-
-	hideError: function() {
-		AuthorInfo.errorDiv.style.display = "none";
-		AuthorInfo.errorDiv.innerHTML = "";
 	}
-};
+}
+
+AuthorInfo.showAllAuthors = function() {
+	AuthorInfo.loadAuthors(0);
+}
+
+AuthorInfo.checkResponse = function(xhr) {
+	if (xhr.readyState==4){
+		if (xhr.status==200) {
+			return true;
+		} else {
+			AuthorInfo.showError(xhr.statusText);
+		}
+	} else {
+		AuthorInfo.showError(xhr.statusText);
+	}
+}
+
+AuthorInfo.showError = function(msg) {
+	AuthorInfo.errorDiv.style.display = "block";
+	AuthorInfo.errorDiv.innerHTML = "<p class='authorerror'>Error loading authors: " + msg + 
+		" - <a href='javascript:AuthorInfo.hideError();'>close</a></p>";
+}
+
+AuthorInfo.hideError = function() {
+	AuthorInfo.errorDiv.style.display = "none";
+	AuthorInfo.errorDiv.innerHTML = "";
+}
--- wpi/extensions/AuthorInfo/AuthorInfo.php
+++ wpi/extensions/AuthorInfo/AuthorInfo.php
@@ -1,70 +1,200 @@
 <?php
-//Register the extension
-$wgExtensionCredits['validextensionclass'][] = array(
-	'path' => __FILE__,
-	'name' => 'AuthorInfo',
-	'author' =>'Thomas Kelder', 
-	'url' => 'https://www.mediawiki.org/wiki/Extension:AuthorInfo', 
-	'description' => 'Displays a list of authors that contributed to an article, sorted by number of edits.'
-);
+$wgExtensionFunctions[] = "wfAuthorInfo";
 
-//Create a module to load the javascript and css used by this extension
-$wgResourceModules['ext.AuthorInfo'] = array(
-	'scripts' => array( 'wpi/extensions/AuthorInfo/AuthorInfo.js' ),
-	'styles' => array( 'wpi/extensions/AuthorInfo/AuthorInfo.css' )
-);
+$wgAjaxExportList[] = "jsGetAuthors";
 
-//Load the classes used by this extension
-$dir = dirname(__FILE__) . '/';
-$wgAutoloadClasses['AuthorInfoList'] = $dir . 'AuthorInfo.body.php';
-$wgAutoloadClasses['AuthorInfo'] = $dir . 'AuthorInfo.body.php';
+$wfAuthorInfoPath = WPI_URL . "/extensions/AuthorInfo";
 
-//Register extension hooks
-$wgHooks['ParserFirstCallInit'][] = 'AuthorInfoExtension::init';
-//Register ajax functions
-$wgAjaxExportList[] = "AuthorInfoExtension::jsGetAuthors";
+function wfAuthorInfo() {
+    global $wgParser;
+    $wgParser->setHook( "AuthorInfo", "renderAuthorInfo" );
+}
+
+function renderAuthorInfo($input, $argv, &$parser) {
+	global $wfAuthorInfoPath;
+	$parser->disableCache();
+	
+	$limit = htmlentities($argv["limit"]);
+	if(!$limit) $limit = 0;
+	$bots = htmlentities($argv["bots"]);
+	if(!$bots) $bots = false;
+
+	//Add CSS
+	//Hack to add a css that's not in the skins directory
+	global $wgStylePath, $wgOut;
+	$oldStylePath = $wgStylePath;
+	$wgStylePath = $wfAuthorInfoPath;
+	$wgOut->addStyle("AuthorInfo.css");
+	$wgStylePath = $oldStylePath;
+	
+	$html = "<script type=\"text/javascript\" src=\"$wfAuthorInfoPath/AuthorInfo.js\"></script>\n";
+	$id = $parser->getTitle()->getArticleId();
+	$html .= "<div id='authorInfoContainer'></div><script type=\"text/javascript\">" .
+		"AuthorInfo.init('authorInfoContainer', '$id', '$limit', '$bots');</script>";
+	return $html;
+}
 
 
-class AuthorInfoExtension {
-	static function init(Parser &$parser) {
-		$parser->setHook( 'authorInfo', 'AuthorInfoExtension::render' );
-        return true;
+/**
+ * Called from javascript to get the author list.
+ * @param $pageId The id of the page to get the authors for.
+ * @param $limit Limit the number of authors to query. Leave empty to get all authors.
+ * @param $includeBots Whether to include users marked as bot.
+ * @return An xml document containing all authors for the given page
+ */
+function jsGetAuthors($pageId, $limit = '', $includeBots = false) {
+	$title = Title::newFromId($pageId);
+	if($includeBots === 'false') $includeBots = false;
+	$authorList = new AuthorInfoList($title, $limit, $includeBots);
+	$doc = $authorList->getXml();
+	$resp = new AjaxResponse($doc->saveXML());
+	$resp->setContentType("text/xml");
+	return $resp;
+}
+
+class AuthorInfoList {
+	private $title;
+	private $limit;
+	private $showBots;
+	
+	private $authors;
+	
+	public function __construct($title, $limit = '', $showBots = false) {
+		$this->title = $title;
+		if($limit) $this->limit = $limit + 1;
+		$this->showBots = $showBots;
+		$this->load();
 	}
 	
-	static function render($input, array $args, Parser $parser, PPFrame $frame) {
-		global $wgOut;
-		$wgOut->addModules('ext.AuthorInfo');
-		 
-		$parser->disableCache();
-
-		$limit = htmlentities($args["limit"]);
-		if(!$limit) $limit = 0;
-		$bots = htmlentities($args["bots"]);
-		if(!$bots) $bots = false;
-
-		$id = $parser->getTitle()->getArticleId();
-		$html = "<div id='authorInfoContainer'></div><script type=\"text/javascript\">" .
-		"$(document).ready(function() { AuthorInfo.init('authorInfoContainer', '$id', '$limit', '$bots') });</script>";
-		return $html;
+	private function load() {
+		$dbr = wfGetDB( DB_SLAVE );
+		$limit = '';
+		if($this->limit) {
+			$limit = "LIMIT 0, {$this->limit}";
+		}
+		
+		//Get users for page
+		$page_id = $this->title->getArticleId();
+		$query = "SELECT DISTINCT(rev_user) FROM revision WHERE " .
+			"rev_page = {$page_id} $limit";
+			
+		$res = $dbr->query($query);
+		$this->authors = array();
+		while($row = $dbr->fetchObject( $res )) {
+			$user = User::newFromId($row->rev_user);
+			if($user->isAnon()) continue; //Skip anonymous users
+			if(!$user->isAllowed("bot") || $this->showBots) {
+				$this->authors[] = new AuthorInfo($user, $this->title);
+			}
+		}
+		
+		//Sort the authors by editCount
+		usort($this->authors, "AuthorInfo::compareByEdits");
+		$dbr->freeResult( $res );
 	}
 	
 	/**
-	 * Called from javascript to get the author list.
-	 * @param $pageId The id of the page to get the authors for.
-	 * @param $limit Limit the number of authors to query. Leave empty to get all authors.
-	 * @param $includeBots Whether to include users marked as bot.
-	 * @return An xml document containing all authors for the given page
+	 * Render the author list.
+	 * @return A HTML snipped containing the author list
 	 */
-	static function jsGetAuthors($pageId, $limit = '', $includeBots = false) {
-		$title = Title::newFromId($pageId);
-		if($includeBots === 'false') $includeBots = false;
-		$authorList = new AuthorInfoList($title, $limit, $includeBots);
-		$doc = $authorList->getXml();
-		$resp = new AjaxResponse($doc->saveXML());
-		$resp->setContentType("text/xml");
-		return $resp;
+	public function renderAuthorList() {
+		$html = '';
+		foreach($this->authors as $a) {
+			$html .= $a->renderAuthor() . ", ";
+		}
+		return substr($html, 0, -2);
 	}
+	
+	/**
+	 * Get an XML document containing the author info
+	 */
+	public function getXml() {
+		$doc = new DOMDocument();
+		$root = $doc->createElement("AuthorList");
+		$doc->appendChild($root);
+		
+		foreach($this->authors as $a) {
+			$a->addXml($doc, $root);
+		}
+		return $doc;
+	}
+}
 
+class AuthorInfo {
+	private $title;
+	private $user;
+	private $editCount;
+	
+	public function __construct($user, $title) {
+		$this->title = $title;
+		$this->user = $user;
+		$this->load();
+	}
+	
+	public function getEditCount() {
+		return $this->editCount;
+	}
+	
+	private function load() {
+		$dbr = wfGetDB( DB_SLAVE );
+		$query = "SELECT COUNT(rev_user) AS editCount FROM revision " .
+			"WHERE rev_user={$this->user->getId()} " .
+			"AND rev_page={$this->title->getArticleId()}";
+		$res = $dbr->query($query);
+		$row = $dbr->fetchObject( $res );
+		$this->editCount = $row->editCount;
+		$dbr->freeResult( $res );
+	}
+	
+	public function getDisplayName() {
+		$name = $this->user->getRealName();
+		
+		//Filter out email addresses
+		if(preg_match("/^[-_a-z0-9\'+*$^&%=~!?{}]++(?:\.[-_a-z0-9\'+*$^&%=~!?{}]+)*+@(?:(?![-.])[-a-z0-9.]+(?<![-.])\.[a-z]{2,6}|\d{1,3}(?:\.\d{1,3}){3})(?::\d++)?$/iD", $name)) {
+			$name = ''; //use username instead
+		}
+		if(!$name) $name = $this->user->getName();
+		return $name;
+	}
+	
+	private function getAuthorLink() {
+		global $wgScriptPath;
+		$title = Title::newFromText('User:' . $this->user->getTitleKey());
+		$href = $title->getFullUrl();
+		return $href;
+	}
+	
+	/**
+	 * Creates the HTML code to display a single
+	 * author
+	 */
+	public function renderAuthor() {
+		$name = $this->getDisplayName();
+		$href = $this->getAuthorLink();
+		$link = "<A href=\"$href\" title=\"Number of edits: {$this->editCount}\">" .
+			htmlspecialchars($name) . "</A>";
+		return $link;
+	}
+	
+	/**
+	 * Add an XML node for this author to the
+	 * given node.
+	 */
+	public function addXml($doc, $node) {
+		$e = $doc->createElement("Author");
+		$e->setAttribute("Name", $this->getDisplayName());
+		$e->setAttribute("EditCount", $this->editCount);
+		$e->setAttribute("Url", $this->getAuthorLink());
+		$node->appendChild($e);
+	}
+	
+	public static function compareByEdits($a1, $a2) {
+		$c = $a2->getEditCount() - $a1->getEditCount();
+		if($c == 0) { //If equal edits, compare by realname
+			$c = strcasecmp($a1->getDisplayName(), $a2->getDisplayName());
+		}
+		return $c;
+	}
 }
 
 ?>

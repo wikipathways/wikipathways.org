324043b0b7323a0d9cbcf598de52f01e4148d4d2
martijn <martijn@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2012-09-10 22:53:28 +0000

    Revert "added id back to pathwayThumb"
    This reverts commit 0e8b758a4cc3b5e10f1306b4a8b936ff1370e939.

--- wpi/extensions/pathwayThumb.php
+++ wpi/extensions/pathwayThumb.php
@@ -128,7 +128,7 @@ function makeThumbLink2($pathway, $frameParams = array(),
 	if ( $page ) {
 		$url = wfAppendQuery( $url, 'page=' . urlencode( $page ) );
 	}
-	$s = "<div class=\"thumb t{$fp['align']}\"><div class=\"thumbinner\" style=\"width:{$outerWidth}px;\" id=\"${fp['id']}>";
+	$s = "<div class=\"thumb t{$fp['align']}\"><div class=\"thumbinner\" style=\"width:{$outerWidth}px;\">";
 	if ( !$exists ) {
 		$s .= Linker::makeBrokenImageLinkObj( $title, $fp['title'], '', '', '', $time == true );
 		$zoomIcon = '';
@@ -182,5 +182,79 @@ function createEditCaption($pathway) {
 	$drop = '<div style="float:right;">' . $drop . '</div>';
 	return $caption . $drop;
 }
+
+    /** MODIFIED FROM Linker.php
+        * Make HTML for a thumbnail including image, border and caption
+        * $img is an Image object
+        */
+    function makeThumbLinkObj( $pathway, $label = '', $href = '', $alt, $align = 'right', $id = 'thumb', $boxwidth = 180, $boxheight=false, $framed=false ) {
+            global $wgStylePath, $wgContLang;
+
+			$pathway->updateCache(FILETYPE_IMG);
+			$img = RepoGroup::singleton()->getLocalRepo()->newFile($pathway->getFileTitle(FILETYPE_IMG));
+            //$img = new Image($pathway->getFileTitle(FILETYPE_IMG));
+            $img->loadFromFile();
+            
+            $imgURL = $img->getURL();
+
+            $thumbUrl = '';
+            $error = '';
+
+            $width = $height = 0;
+            if ( $img->exists() ) {
+                    $width  = $img->getWidth();
+                    $height = $img->getHeight();
+            }
+            if ( 0 == $width || 0 == $height ) {
+                    $width = $height = 180;
+            }
+            if ( $boxwidth == 0 ) {
+                    $boxwidth = 180;
+            }
+            if ( $framed ) {
+                    // Use image dimensions, don't scale
+                    $boxwidth  = $width;
+                    $boxheight = $height;
+                    $thumbUrl  = $img->getViewURL();
+            } else {
+                    if ( $boxheight === false ) $boxheight = -1;
+                    $thumb = $img->getThumbnail( $boxwidth, $boxheight );
+                    if ( $thumb ) {
+                            $thumbUrl = $thumb->getUrl();
+                            $boxwidth = $thumb->width;
+                            $boxheight = $thumb->height;
+                    } else {
+                            $error = $img->getLastError();
+                    }
+            }
+            $oboxwidth = $boxwidth + 2;
+
+            $more = htmlspecialchars( wfMsg( 'thumbnail-more' ) );
+            $magnifyalign = $wgContLang->isRTL() ? 'left' : 'right';
+            $textalign = $wgContLang->isRTL() ? ' style="text-align:right"' : '';
+
+            $s = "<div id=\"{$id}\" class=\"thumb t{$align}\"><div class=\"thumbinner\" style=\"width:{$oboxwidth}px;\">";
+            if( $thumbUrl == '' ) {
+                    // Couldn't generate thumbnail? Scale the image client-side.
+                    $thumbUrl = $img->getViewURL();
+                    if( $boxheight == -1 ) {
+                            // Approximate...
+                            $boxheight = intval( $height * $boxwidth / $width );
+                    }
+            }
+            if ( $error ) {
+                    $s .= htmlspecialchars( $error );
+            } elseif( !$img->exists() ) {
+                    $s .= "Image does not exist";
+            } else {
+                    $s .= '<a href="'.$href.'" class="internal" title="'.$alt.'">'.
+                            '<img src="'.$thumbUrl.'" alt="'.$alt.'" ' .
+                            'width="'.$boxwidth.'" height="'.$boxheight.'" ' .
+                            'longdesc="'.$href.'" class="thumbimage" /></a>';
+            }
+            $s .= '  <div class="thumbcaption"'.$textalign.'>'.$label."</div></div></div>";
+            return str_replace("\n", ' ', $s);
+            //return $s;
+    }
 
 ?>

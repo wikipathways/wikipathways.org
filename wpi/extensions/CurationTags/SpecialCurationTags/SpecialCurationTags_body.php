94aa3502418c110e993c69bea03650969da6e399
mah <mah@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2013-03-30 21:45:07 +0000

    Add <tr> to the beginning of the header row and “Last Edit” header

--- wpi/extensions/CurationTags/SpecialCurationTags/SpecialCurationTags_body.php
+++ wpi/extensions/CurationTags/SpecialCurationTags/SpecialCurationTags_body.php
@@ -61,7 +61,7 @@ class SpecialCurationTags extends SpecialPage {
 			);
 			$wgOut->addHTML("<p><a href='$url'>back</a></p>");
 			$wgOut->addHTML("<table class='prettytable sortable'><tbody>");
-			$wgOut->addHTML("<th>Pathway name<th>Organism<th>Tagged by<th>");
+			$wgOut->addHTML("<tr><th>Pathway name<th>Organism<th>Tagged by<th>Last Edit");
 			if($useRev) {
 				$wgOut->addHTML("<th>Applies to latest revision");
 			}

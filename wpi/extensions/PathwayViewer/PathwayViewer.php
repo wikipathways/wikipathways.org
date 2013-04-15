4cd7336981b1800cc56794f8175a88e04e0cd538
mah <mah@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2013-04-15 00:19:09 +0000

    * Fix up multiple headers being included * Remove some ending php ?>

--- wpi/extensions/PathwayViewer/PathwayViewer.php
+++ wpi/extensions/PathwayViewer/PathwayViewer.php
@@ -10,89 +10,88 @@ $wgExtensionFunctions[] = 'wfPathwayViewer';
 $wgHooks['LanguageGetMagic'][]  = 'wfPathwayViewer_Magic';
 
 function wfPathwayViewer() {
-    global $wgParser;
-    $wgParser->setFunctionHook( "PathwayViewer", "displayPathwayViewer" );
+	global $wgParser;
+	$wgParser->setFunctionHook( "PathwayViewer", "displayPathwayViewer" );
 }
 
 function wfPathwayViewer_Magic( &$magicWords, $langCode ) {
-        $magicWords['PathwayViewer'] = array( 0, 'PathwayViewer' );
-        return true;
+	$magicWords['PathwayViewer'] = array( 0, 'PathwayViewer' );
+	return true;
 }
 
 function displayPathwayViewer(&$parser, $pwId, $imgId) {
 	global $wgOut, $wgStylePath, $wfPathwayViewerPath, $wpiJavascriptSources, $wgScriptPath,
 		$wpiJavascriptSnippets, $jsRequireJQuery, $wpiAutoStartViewer;
-	
+
 	$jsRequireJQuery = true;
-	
+
 	try {
 		$parser->disableCache();
 
 		//Force flash renderer
 		//<meta name="svg.render.forceflash" content="true">
 		$wgOut->addMeta('svg.render.forceflash', 'true');
-		
+
 		//Add javascript dependencies
 		XrefPanel::addXrefPanelScripts();
 		$wpiJavascriptSources = array_merge($wpiJavascriptSources, PathwayViewer::getJsDependencies());
-		
+
 		$script = "PathwayViewer_basePath = '" . $wfPathwayViewerPath . "/';";
 		$wpiJavascriptSnippets[] = $script;
 
 		$revision = $_REQUEST['oldid'];
 		$pvPwAdded[] = $pwId . '@' . $revision;
-		
+
 		$pathway = Pathway::newFromTitle($pwId);
 		if($revision) {
 			$pathway->setActiveRevision($revision);
 		}
 		$svg = $pathway->getFileURL(FILETYPE_IMG);
 		$gpml = $pathway->getFileURL(FILETYPE_GPML);
-		
+
 		$dostart = ',start: true';
 		$start = $dostart; //Autostart by default
-		
+
 		//First switch by variable in pass.php
 		if(!$wpiAutoStartViewer) $start = $dostart;
-		
+
 		//Allow override via get parameter
 		$getStart = $_GET['startViewer'];
 		if($getStart == 'false' || $getStart == '0') $start = '';
 		if($getStart == 'true' || $getStart == '1') $start = $dostart;
-		
+
 		$script = <<<SCRIPT
-	var viewer = new PathwayViewer({
-		imageId: "$imgId",
-		svgUrl: "$svg",
-		gpmlUrl: "$gpml"$start
-	});
-	PathwayViewer_viewers.push(viewer);
-SCRIPT;
+			var viewer = new PathwayViewer({
+				  imageId: "$imgId",
+						svgUrl: "$svg",
+						gpmlUrl: "$gpml"$start
+						});
+		PathwayViewer_viewers.push(viewer);
+		SCRIPT;
 		$script = "<script type=\"{$wgJsMimeType}\">" . $script . "</script>\n";
 		return array($script, 'isHTML'=>1, 'noparse'=>1);
 	} catch(Exception $e) {
-          return "invalid pathway title: $e";
-     }
+		return "invalid pathway title: $e";
+	}
 	return true;
 }
 
 class PathwayViewer {
 	static function getJsDependencies() {
 		global $jsSvgWeb, $wgScriptPath, $wfPathwayViewerPath;
-		
+
 		$scripts = array(
 			"$wgScriptPath/wpi/js/jquery/plugins/jquery.mousewheel.js",
 			"$wgScriptPath/wpi/js/jquery/plugins/jquery.layout.min-1.3.0.js",
 			"$wfPathwayViewerPath/pathwayviewer.js",
 			"$wfPathwayViewerPath/highlightByElement.js"
 		);
-		
+
 		//Do not load svgweb when using HTML5 version of svg viewer (IE9)
 		if(browser_detection('ie_version') != 'ie9x') {
 			array_unshift($scripts, $jsSvgWeb);
 		}
-		
+
 		return $scripts;
 	}
 }
-?>
--- wpi/extensions/XrefPanel.php
+++ wpi/extensions/XrefPanel.php
@@ -14,7 +14,7 @@ class XrefPanel {
 	static function xref() {
 		global $wgParser;
 		$wgParser->setHook( "Xref", "XrefPanel::renderXref" );
-	
+
 		wpiAddXrefPanelScripts();
 	}
 
@@ -32,31 +32,31 @@ class XrefPanel {
 		$html = $text . " <img title='Show additional info and linkouts' style='cursor:pointer;' onload='$fun' src='$url'/>";
 		return $html;
 	}
-	
+
 	static function getJsDependencies() {
 		global $jsJQueryUI, $wgScriptPath;
-		
+
 		$js = array(
 			"$wgScriptPath/wpi/js/xrefpanel.js",
 			$jsJQueryUI
 		);
-		
+
 		return $js;
 	}
-	
+
 	static function getJsSnippets() {
 		global $wpiXrefPanelDisableAttributes, $wpiBridgeUrl,
 			$wpiBridgeUseProxy;
-		
+
 		$js = array();
-		
+
 		$js[] = 'XrefPanel_searchUrl = "' . SITE_URL . '/index.php?title=Special:SearchPathways&doSearch=1&ids=$ID&codes=$DATASOURCE&type=xref";';
 		if($wpiXrefPanelDisableAttributes) {
 			$js[] = 'XrefPanel_lookupAttributes = false;';
 		}
-		
+
 		$bridge = "XrefPanel_dataSourcesUrl = '" . WPI_CACHE_URL . "/datasources.txt';\n";
-		
+
 		if($wpiBridgeUrl !== false) { //bridgedb web service support can be disabled by setting $wpiBridgeDb to false
 			if(!isset($wpiBridgeUrl) || $wpiBridgeUseProxy) {
 				//Point to bridgedb proxy by default
@@ -66,25 +66,23 @@ class XrefPanel {
 			}
 		}
 		$js[] = $bridge;
-		
+
 		return $js;
 	}
-	
+
 	static function addXrefPanelScripts() {
 		global $wpiJavascriptSources, $wpiJavascriptSnippets, $cssJQueryUI, $wgScriptPath, $wgStylePath, $wgOut, $jsRequireJQuery;
-		
+
 		$jsRequireJQuery = true;
-		
+
 		//Add CSS
 		//Hack to add a css that's not in the skins directory
 		$oldStylePath = $wgStylePath;
 		$wgStylePath = dirname($cssJQueryUI);
 		$wgOut->addStyle(basename($cssJQueryUI));
 		$wgStylePath = $oldStylePath;
-	
+
 		$wpiJavascriptSources = array_merge($wpiJavascriptSources, self::getJsDependencies());
 		$wpiJavascriptSnippets = array_merge($wpiJavascriptSnippets, self::getJsSnippets());
 	}
 }
-
-?>

51dff218dc1377a42bbe4d5cdb2405d0636beba3
mah <mah@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2013-04-19 01:23:13 +0000

    Remove references to categories

--- wpi/pathway_content_flatfile.php
+++ wpi/pathway_content_flatfile.php
@@ -7,7 +7,7 @@ require_once('search.php');
 //Get commandline options
 $o = getopt("s:f:m:");
 
-//Get requested species 
+//Get requested species
 $species = $o["s"]; //'Zea mays'; //$_REQUEST['species'];
 
 //Get output format(tab or html)
@@ -37,7 +37,7 @@ function returnCached() {
 
 function generateContent($species) {
 	global $outputFormat, $cacheFile;
-	
+
 	$fh = fopen($cacheFile, 'w');
 
 	error_reporting(0);
@@ -54,7 +54,7 @@ function generateContent($species) {
 		"CAS",
 		"ChEBI",
 	);
-	
+
 
 	// Print header
 	//NOTE: Model Organism Databases = HUGO, MGI, RGD, ZFIN, FlyBase, WormBase, SGD
@@ -63,10 +63,10 @@ function generateContent($species) {
 		foreach($datasourceList as $s) {
 			$sysCols .= "<TD>$s</TD>";
 		}
-	
+
 		fwrite($fh, "<html><table border=1 cellpadding=3>
 		<tr bgcolor=\"#CCCCFF\" font><td>Pathway Name</td><td>Organism</td><td>Gene Ontology</td><td>Url to WikiPathways</td><td>Last Changed</td><td>Last Revision</td><td>Author</td><td>Count</td>$sysCols</tr>\n");
-	
+
 	} elseif ($outputFormat == 'excel'){
 		//TODO (see Pear module for spreadsheet writer)
 		fwrite($fh, "Not available yet...\n");
@@ -77,32 +77,32 @@ function generateContent($species) {
 		}
 		//print header
 		fwrite($fh, "Pathway Name\tOrganism\tGene Ontology\tUrl to WikiPathways\tLast Changed\tLast Revision\tAuthor\tCount$sysCols\n");
-	} 
+	}
 
-    	// Loop through all species, if requested
+	// Loop through all species, if requested
 	// THIS DOESN'T WORK: TAKES TOO LONG?
-    	//if ($species == 'All') {
-    	//	foreach(Pathway::getAvailableSpecies() as $species) {
+	//if ($species == 'All') {
+	//	foreach(Pathway::getAvailableSpecies() as $species) {
 	//		processPathways($species, $fh, $datasourceList);
 	//	}
 	//} else {
-		processPathways($species, $fh, $datasourceList);
+	processPathways($species, $fh, $datasourceList);
 	//}
 
-        //Print footer
-        if ($outputFormat =='html'){
-                fwrite($fh, "</table></html>");
-        } elseif ($outputFormat == 'excel'){
-                //TODO
-        } else {
+	//Print footer
+	if ($outputFormat =='html'){
+		fwrite($fh, "</table></html>");
+	} elseif ($outputFormat == 'excel'){
+		//TODO
+	} else {
 
-        }
+	}
 
-        fclose($fh);
+	fclose($fh);
 }
 
-function processPathways($species, $fh, $datasourceList) { 
-        global $outputFormat, $cacheFile, $mappingPref;
+function processPathways($species, $fh, $datasourceList) {
+	global $outputFormat, $cacheFile, $mappingPref;
 
 	$pathwayList = Pathway::getAllPathways($species);
 
@@ -115,12 +115,12 @@ function processPathways($species, $fh, $datasourceList) {
 		$page_id = $pathway->getPageIdDB();
 		if (in_array($page_id, CurationTag::getPagesForTag('Curation:Tutorial'))) continue;
 		if (in_array($page_id, CurationTag::getPagesForTag('Curation:ProposedDeletion'))) continue;
-                if (in_array($page_id, CurationTag::getPagesForTag('Curation:Stub'))) continue;
-                if (in_array($page_id, CurationTag::getPagesForTag('Curation:InappropriateContent'))) continue;
-                if (in_array($page_id, CurationTag::getPagesForTag('Curation:UnderConstruction'))) continue;
+		if (in_array($page_id, CurationTag::getPagesForTag('Curation:Stub'))) continue;
+		if (in_array($page_id, CurationTag::getPagesForTag('Curation:InappropriateContent'))) continue;
+		if (in_array($page_id, CurationTag::getPagesForTag('Curation:UnderConstruction'))) continue;
 		//Exclude deleted and private pathways
 		if($pathway->isDeleted() || !$pathway->isPublic()) continue;
-	
+
 		try {
 			$modTime = $pathway->getGpmlModificationTime();
 			$url = $pathway->getFullUrl();
@@ -138,34 +138,31 @@ function processPathways($species, $fh, $datasourceList) {
 			$author = implode(', ', $authors);
 			$lastRevision = $pathway->getLatestRevision();
 
-			$catArray = implode(", ", $pathway->getCategoryHandler()->getCategories());
-			perlChop($categories);
-
 			// Print pathways data
 			if ($outputFormat =='html'){
-				fwrite($fh, "<tr><td>".$pathwayName."</td><td>".$species."</td><td>".$categories."&nbsp</td><td>".$url."</td><td>".$modTime."</td><td>".$lastRevision."</td><td>".$author."&nbsp</td><td>");
+				fwrite($fh, "<tr><td>".$pathwayName."</td><td>".$species."</td><td>&nbsp</td><td>".$url."</td><td>".$modTime."</td><td>".$lastRevision."</td><td>".$author."&nbsp</td><td>");
 			}
 			elseif ($outputFormat == 'excel'){
 				//TODO
 			}
 			else {
-				fwrite($fh, $pathwayName."\t".$species."\t".$categories."\t".$url."\t".$modTime."\t".$lastRevision."\t".$author."\t");
+				fwrite($fh, $pathwayName."\t".$species."\t\t".$url."\t".$modTime."\t".$lastRevision."\t".$author."\t");
 			}
-			
+
 			//Count original datanodes
-                        try
-                        {
-                          $xrefs = $pathway->getUniqueXrefs();
-                        }
-                          catch (Exception $e)
-                        {
-                        // we can safely ignore exceptions
-                        // erroneous pathways simply won't get counted
-                        }
+			try
+				{
+					$xrefs = $pathway->getUniqueXrefs();
+				}
+			catch (Exception $e)
+				{
+					// we can safely ignore exceptions
+					// erroneous pathways simply won't get counted
+				}
 			$xrefCount = count($xrefs);
 
 			// Print xref translations
-                        $datasourceXrefMap = array();
+			$datasourceXrefMap = array();
 			$updatedDatasourceList = array();
 			foreach($datasourceList as $s) {
 				if($s === "Ensembl"){
@@ -181,75 +178,75 @@ function processPathways($species, $fh, $datasourceList) {
 						$datasourceXrefMap[$s] = ' ';
 						continue;
 					}
-				}	
+				}
 				$code = DataSource::getCode($s);
 
 				## id mapping = off
 				if($mappingPref == "off"){
-		                        try
-               		         	{
-                       		         $xrefs = $pathway->getUniqueXrefs();
-                                        }
-                                        catch (Exception $e)
-                                        {
-                                // we can safely ignore exceptions
-                                // erroneous pathways simply won't get counted
-                                        }
+					try
+						{
+							$xrefs = $pathway->getUniqueXrefs();
+						}
+					catch (Exception $e)
+						{
+							// we can safely ignore exceptions
+							// erroneous pathways simply won't get counted
+						}
 
 					$tmp = "";
-                       		        foreach ($xrefs as $xref){
-                                	        $id = $xref->getId();
-                                       	 	$db = $xref->getSystem(); # system name
+					foreach ($xrefs as $xref){
+						$id = $xref->getId();
+						$db = $xref->getSystem(); # system name
 						if ($db == $s){
 							if ($id && $id != '' && $id != ' '){
-                                                   		$tmp .= $id .',';
-                                                	}
-                                        	}
-                               	 	}	
+								$tmp .= $id .',';
+							}
+						}
+					}
 					perlChop($tmp);
 					$datasourceXrefMap[$s] = $tmp;
 					$updatedDatasourceList[] = $s;
 				} else {
-				## id mapping = on
+					## id mapping = on
 					try {
-						$xrefList = PathwayIndex::listPathwayXrefs($pathway, $code, 'FALSE');			
-			        	} catch(Exception $e) {
-         		        		throw new WSFault("Receiver", "Unable to process request: " . $e);
-        				}
-			
+						$xrefList = PathwayIndex::listPathwayXrefs($pathway, $code, 'FALSE');
+					} catch(Exception $e) {
+						throw new WSFault("Receiver", "Unable to process request: " . $e);
+					}
+
 					$tmp = "";
 					foreach($xrefList as $xref) {
 						$tmp .= $xref . ',';
 					}
- 					perlChop($tmp); //remove final comma from generated list
+					perlChop($tmp); //remove final comma from generated list
 					$datasourceXrefMap[$s] = $tmp;
 					$updatedDatasourceList[] = $s;
 				}
-			}		
+			}
 			//array_walk($datasourceXrefMap, 'perlChop');
-		
+
 			//Print gene content data
 			if ($outputFormat =='html') {
-				fwrite($fh, $xrefCount); 
+				fwrite($fh, $xrefCount);
 				foreach($updatedDatasourceList as $s) {
-					//append with space character toprovide for empty cells in html table 
+					//append with space character toprovide for empty cells in html table
 					fwrite($fh, "<TD>{$datasourceXrefMap[$s]}&nbsp</TD>");
 				}
 				fwrite($fh, "</TR>");
 			} elseif ($outputFormat == 'excel'){
 				//TODO
 			} else {
-				fwrite($fh, $xrefCount); 
+				fwrite($fh, $xrefCount);
 				foreach($updatedDatasourceList as $s) {
-					//append with space character toprovide for empty cells in html table 
+					//append with space character toprovide for empty cells in html table
 					fwrite($fh, "\t{$datasourceXrefMap[$s]}");
 				}
 				fwrite($fh, "\n");
 			}
 
 		} catch (Exception $e) {
-		 	// we can safely ignore exceptions
-		 	// erroneous pathways simply won't get processed
+			// we can safely ignore exceptions
+			// erroneous pathways simply won't get processed
 		}
 	}
 
@@ -257,8 +254,6 @@ function processPathways($species, $fh, $datasourceList) {
 
 function perlChop(&$string){
 	$endchar = substr("$string", strlen("$string") - 1, 1);
-	$string = substr("$string", 0, -1); 
+	$string = substr("$string", 0, -1);
 	return $endchar;
 }
-
-?>

c33248c85ddca7f5133ccebb2293fb20a32c6ee4
mah <mah@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2013-04-20 22:13:17 +0000

    only use declared globals

--- wpi/extensions/otag/otags_main.php
+++ wpi/extensions/otag/otags_main.php
@@ -1,88 +1,88 @@
 <?php
 
 if ( !defined( 'MEDIAWIKI' ) ) {
-    echo "This file is part of MediaWiki, it is not a valid entry point.\n";
-    exit( 1 );
+	echo "This file is part of MediaWiki, it is not a valid entry point.\n";
+	exit( 1 );
 }
 $opath = WPI_URL . "/extensions/otag" ;
 $wgExtensionFunctions[] = "wfotag";
 
 function wfotag() {
-    global $wgHooks;
+	global $wgHooks;
 	global $wgParser;
-    $wgHooks['ParserAfterTidy'][]='oheader';
+	$wgHooks['ParserAfterTidy'][]='oheader';
 	$wgParser->setHook( "OntologyTags", "ofunction" );
 }
 
 function oheader(&$parser, &$text)
 {
-$text = preg_replace(
-            '/<!-- ENCODED_CONTENT ([0-9a-zA-Z\\+]+=*) -->/e',
-            'base64_decode("$1")',
-            $text
+	$text = preg_replace(
+		'/<!-- ENCODED_CONTENT ([0-9a-zA-Z\\+]+=*) -->/e',
+		'base64_decode("$1")',
+		$text
 
-        );
-        return true;
+	);
+	return true;
 }
 
 function ofunction( $input, $argv, &$parser ) {
-     global $wgTitle , $wgOut, $opath, $wgOntologiesJSON,$wgStylePath;
-     $oldStylePath = $wgStylePath;
-	 $wgStylePath = $opath . "/css/";
+	global $wgTitle , $wgOut, $opath, $wgOntologiesJSON,$wgStylePath, $wgJsMimeType
+		$oldStylePath = $wgStylePath;
+	$wgStylePath = $opath . "/css/";
 
-     $title = $parser->getTitle();
-     $loggedIn = $title->userCan('edit') ? 1 : 0;
-     
-     if($loggedIn)
-     {
-        $wgOut->addScript('<script type="text/javascript" src="' . $opath . '/js/yui2.7.0.allcomponents.js"></script>');
-        $wgOut->addStyle("yui2.7.0.css");
-     }
-     else
-     {
-        $wgOut->addScript('<script type="text/javascript" src="' . $opath . '/js/yui2.7.0.mincomponents.js"></script>');
-     }
+	$title = $parser->getTitle();
+	$loggedIn = $title->userCan('edit') ? 1 : 0;
 
-     $wgOut->addStyle("otag.css");
-     $wgStylePath = $oldStylePath;
+	if($loggedIn)
+		{
+			$wgOut->addScript('<script type="text/javascript" src="' . $opath . '/js/yui2.7.0.allcomponents.js"></script>');
+			$wgOut->addStyle("yui2.7.0.css");
+		}
+	else
+		{
+			$wgOut->addScript('<script type="text/javascript" src="' . $opath . '/js/yui2.7.0.mincomponents.js"></script>');
+		}
 
-     $wgOut->addScript(
+	$wgOut->addStyle("otag.css");
+	$wgStylePath = $oldStylePath;
+
+	$wgOut->addScript(
 		"<script type=\"{$wgJsMimeType}\">" .
 		"var opath=\"$opath\";" .
-                "var otagloggedIn = \"$loggedIn\";" .
-                "var ontologiesJSON = '$wgOntologiesJSON';" .
+		"var otagloggedIn = \"$loggedIn\";" .
+		"var ontologiesJSON = '$wgOntologiesJSON';" .
 		"</script>\n"
 	);
 
-    if($loggedIn)
+	if($loggedIn)
 
-$output = <<<HTML
+		$output = <<<HTML
 <div id="otagprogress" style="display:none" align='center'><span><img src='$wgStylePath/common/images/progress.gif'> Saving...</span></div>
 <div id="ontologyContainer" class="yui-skin-sam">
-    <div id="ontologyMessage" style="display:none;">No Tags!</div>
-    <div id="ontologyTags" style="display:none;"></div>
-    <div id="ontologyTagDisplay">&nbsp;</div>
-    <a href="javascript:toggleOntologyControls();" id="ontologyEditLabel">Add Ontology tags</a><br /><br />
-    <div id="ontologyEdit" style="display:none;">
-        <div id="myAutoComplete">
-            <input id="ontologyACInput" type="text" onfocus="clearBox();" value="Type Ontology term.."/>
-            <div id="myContainer"></div>
-        </div>
-        <div id="otaghelp" class="otaghelp">To add a tag, either select from the available ontology trees below or type a search term in the search box.</div>
-        <div style="clear:both;"></div>
-        <div id="ontologyTrees"></div>
-    </div>
+	<div id="ontologyMessage" style="display:none;">No Tags!</div>
+	<div id="ontologyTags" style="display:none;"></div>
+	<div id="ontologyTagDisplay">&nbsp;</div>
+	<a href="javascript:toggleOntologyControls();" id="ontologyEditLabel">Add Ontology tags</a><br /><br />
+	<div id="ontologyEdit" style="display:none;">
+		<div id="myAutoComplete">
+			<input id="ontologyACInput" type="text" onfocus="clearBox();" value="Type Ontology term.."/>
+			<div id="myContainer"></div>
+		</div>
+		<div id="otaghelp" class="otaghelp">To add a tag, either select from the available ontology trees below or type a search term in the search box.</div>
+		<div style="clear:both;"></div>
+		<div id="ontologyTrees"></div>
+	</div>
 </div>
 <div style="clear:both;"></div>
 <script type="text/javascript" src="$opath/js/script.js"></script>
 <script type="text/javascript">
-    YAHOO.util.Event.onDOMReady(ontologytree.init, ontologytree,true);
+	YAHOO.util.Event.onDOMReady(ontologytree.init, ontologytree,true);
 </script>
 HTML;
 
-    else
+	else
 
-$output = <<<HTML
+		$output = <<<HTML
 <div id="otagprogress" style="display:none" align='center'><span><img src='$wgStylePath/common/images/progress.gif'> Saving...</span></div>
 <div id="ontologyContainer" class="yui-skin-sam">
 <div id="ontologyMessage" style="display:none;">No Tags!</div>
@@ -92,8 +92,6 @@ $output = <<<HTML
 <script type="text/javascript" src="$opath/js/script.js"></script>
 HTML;
 
-return   '<!-- ENCODED_CONTENT '.base64_encode($output).' -->' ; 
+return   '<!-- ENCODED_CONTENT '.base64_encode($output).' -->' ;
 
 }
-
-?>

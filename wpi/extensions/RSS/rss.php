7b026b8e26f42921884f3e72608b0e44ce63b712
mah <mah@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2013-04-20 20:55:17 +0000

    clean up use of objects

--- wpi/extensions/RSS/rss.php
+++ wpi/extensions/RSS/rss.php
@@ -9,17 +9,17 @@
  * @copyright © mutante, Duesentrieb, Rdb, Mafs, Alxndr, Cmreigrut, K001
  * @link http://www.mediawiki.org/wiki/Extension:RSS Documentation
  *
- * Requires: 
+ * Requires:
  *  # magpie rss parser <http://magpierss.sourceforge.net/>
  *  # iconv <http://www.gnu.org/software/libiconv/>, see also <http://www.php.net/iconv>
  *
  *  07.05.2008 compatible/checked with MediaWiki 1.12
  */
- 
+
 if( !defined( 'MEDIAWIKI' ) ) {
 	die( "This is not a valid entry point.\n" );
 }
- 
+
 $wgExtensionCredits['parserhook'][] = array(
 	'name' => 'RSS feed',
 	'author' => array('mutante', 'Duesentrieb', 'Rdb', 'Mafs', 'Alxndr', 'Wikinaut', 'Cmreigrut', 'K001'),
@@ -27,45 +27,45 @@ $wgExtensionCredits['parserhook'][] = array(
 	'url' => 'http://www.mediawiki.org/wiki/Extension:RSS',
 	'description' => 'Displays an RSS feed on a wiki page'
 );
- 
+
 define('MAGPIE_OUTPUT_ENCODING', 'UTF-8');
- 
+
 #change this according to your magpie installation!
 require_once('extensions/RSS/magpierss/rss_fetch.inc');
- 
+
 // Avoid unstubbing $wgParser too early on modern (1.12+) MW versions, as per r35980
 if ( defined( 'MW_SUPPORTS_PARSERFIRSTCALLINIT' ) ) {
 	$wgHooks['ParserFirstCallInit'][] = 'wfRssExtension';
 } else {
 	$wgExtensionFunctions[] = 'wfRssExtension';
 }
- 
+
 #Extension hook callback function
-function wfRssExtension() { 
+function wfRssExtension() {
 	global $wgParser;
- 
+
 	#Install parser hook for <rss> tags
 	$wgParser->setHook( 'rss', 'renderRss' );
 	return true;
 }
- 
+
 #Parser hook callback function
 function renderRss( $input ) {
 	global $wgOutputEncoding, $wgParser;
- 
+
 	// Kill parser cache
 	$wgParser->disableCache();
- 
+
 	if ( !$input ) return ''; #if <rss>-section is empty, return nothing
 
 	#Parse fields in rss-section
 	$fields = explode( "|", $input );
 	$url = @$fields[0];
- 
+
 	$args = array();
 	for ( $i = 1; $i < sizeof( $fields ); $i++ ) {
 		$f = $fields[$i];
- 
+
 		if ( strpos( $f, "=" ) === false ) $args[strtolower(trim($f))] = false;
 		else {
 			list( $k, $v ) = explode( "=", $f, 2 );
@@ -73,20 +73,20 @@ function renderRss( $input ) {
 			else $args[strtolower(trim($k))] = trim($v);
 		}
 	}
- 
-	#Get charset from argument-array    
+
+	#Get charset from argument-array
 	$charset = @$args['charset'];
 	if( !$charset ) $charset = $wgOutputEncoding;
 	#Get max number of headlines from argument-array
 	$maxheads = @$args['max'];
 	$headcnt = 0;
- 
+
 	#Get short-flag from argument-array
 	#If short is set, no description text is printed
 	if( isset( $args['short'] ) ) $short = true; else $short = false;
 	#Get reverse-flag from argument-array
 	if( isset( $args['reverse'] ) ) $reverse = true; else $reverse = false;
- 
+
 	# Get date format from argument-array
 	if (isset($args["date"])) {
 		$date =  @$args["date"];
@@ -95,36 +95,36 @@ function renderRss( $input ) {
 	}
 	else
 		$date = false;
- 
-	#Get highlight terms from argument array    
+
+	#Get highlight terms from argument array
 	$rssHighlight = @$args['highlight'];
 	$rssHighlight = str_replace( '  ', ' ', $rssHighlight );
 	$rssHighlight = explode( ' ', trim( $rssHighlight ) );
- 
-	#Get filter terms from argument-array    
+
+	#Get filter terms from argument-array
 	$rssFilter = @$args['filter'];
 	$rssFilter = str_replace( '  ', ' ', $rssFilter );
 	$rssFilter = explode( ' ', trim( $rssFilter ) );
- 
+
 	#Filterout terms
 	$rssFilterout = @$args['filterout'];
 	$rssFilterout = str_replace( '  ', ' ', $rssFilterout );
 	$rssFilterout = explode( ' ', trim( $rssFilterout ) );
- 
+
 	#Fetch RSS. May be cached locally.
 	#Refer to the documentation of magpie for details.
 	$rss = @fetch_rss( $url );
- 
+
 	#Check for errors.
-	if ( $rss->ERROR ) {
+	if ( $rss && is_object( $rss ) && $rss->ERROR ) {
 		return "<div>Failed to load RSS feed from $url: ".$rss->ERROR."</div>"; #localize…
 	}
- 
-	if ( !is_array( $rss->items ) ) {
+
+	if ( $rss && is_object( $rss ) && !is_array( $rss->items ) ) {
 		return "<div>Failed to load RSS feed from $url!</div>"; #localize…
 	}
- 
-	#Build title line    
+
+	#Build title line
 	#$title = iconv($charset, $wgOutputEncoding, $rss->channel['title']);
 	#if( $rss->channel['link'] ) $title = "<a href='".$rss->channel['link']."'>$title</a>";
 
@@ -137,27 +137,27 @@ function renderRss( $input ) {
 			break;
 		}
 	}
- 
+
 	#Build items
 	if ( !$short and $description ) { #full item list
 		$output.= '<dl>';
- 
+
 		foreach ( $rss->items as $item ) {
 			$d_text = true;
 			$d_title = true;
- 
+
 			$href = htmlspecialchars( trim( iconv( $charset, $wgOutputEncoding, $item['link'] ) ) );
 			$title = htmlspecialchars( trim( iconv( $charset, $wgOutputEncoding, $item['title'] ) ) );
- 
+
 			if ($date) {
 				$pubdate = trim( iconv( $charset, $wgOutputEncoding, $item['pubdate'] ) );
 				$pubdate = date( $date, strtotime( $pubdate ) );
 			}
- 
+
 			$d_title = wfRssFilter( $title, $rssFilter );
 			$d_title = wfRssFilterout( $title, $rssFilterout );
 			$title = wfRssHighlight( $title, $rssHighlight );
- 
+
 			#Build description text if desired
 			if ( $item['description'] ) {
 				$text = trim( iconv( $charset, $wgOutputEncoding, $item['description'] ) );
@@ -166,12 +166,12 @@ function renderRss( $input ) {
 				$text = str_replace( "\n", ' ', $text );
 				$text = str_replace( "\t", ' ', $text );
 				$text = str_replace( '<br>', '', $text );
- 
+
 				$d_text = wfRssFilter( $text, $rssFilter );
 				$d_text = wfRssFilterout( $text, $rssFilterout );
 				$text = wfRssHighlight( $text, $rssHighlight );
 				$display = $d_text or $d_title;
- 			} else {
+			} else {
 				$text = '';
 				$display = $d_title;
 			}
@@ -183,7 +183,7 @@ function renderRss( $input ) {
 			#Cut off output when maxheads is reached:
 			if ( ++$headcnt == $maxheads ) break;
 		}
- 
+
 		$output.= '</dl>';
 	} else { #short item list
 		## HACKY HACKY HACKY
@@ -208,7 +208,7 @@ function renderRss( $input ) {
 					$output.= " ($pubdate)";
 				}
 				$output.= '</li>';
- 
+
 				$displayed[] = $title;
 				#Cut off output when maxheads is reached:
 				if ( ++$headcnt == $maxheads ) break;
@@ -216,10 +216,10 @@ function renderRss( $input ) {
 		}
 		$output.= '</ul>';
 	}
- 
+
 	return $output;
 }
- 
+
 function wfRssFilter( $text, $rssFilter ) {
 	$display = true;
 	if ( is_array( $rssFilter ) ) {
@@ -236,7 +236,7 @@ function wfRssFilter( $text, $rssFilter ) {
 	}
 	return $display;
 }
- 
+
 function wfRssFilterout( $text, $rssFilterout ) {
 	$display = true;
 	if ( is_array( $rssFilterout ) ) {
@@ -251,19 +251,19 @@ function wfRssFilterout( $text, $rssFilterout ) {
 	}
 	return $display;
 }
- 
+
 function wfRssHighlight( $text, $rssHighlight ) {
 	$i = 0;
 	$starttag = 'v8x5u3t3u8h';
 	$endtag = 'q8n4f6n4n4x';
- 
+
 	$color[] = 'coral';
 	$color[] = 'greenyellow';
 	$color[] = 'lightskyblue';
 	$color[] = 'gold';
 	$color[] = 'violet';
 	$count_color = count( $color );
- 
+
 	if ( is_array( $rssHighlight ) ) {
 		foreach( $rssHighlight as $term ) {
 			if ( $term ) {
@@ -273,13 +273,12 @@ function wfRssHighlight( $text, $rssHighlight ) {
 			}
 		}
 	}
- 
+
 	#To avoid trouble should someone wants to highlight the terms "span", "style", …
 	for ( $i = 0; $i < 5; $i++ ) {
 		$text = preg_replace( "|$starttag"."_".$i."|", "<span style=\"background-color:".$color[$i]."; font-weight: bold;\">", $text );
 		$text = preg_replace( "|$endtag|", '</span>', $text );
 	}
- 
+
 	return $text;
 }
-

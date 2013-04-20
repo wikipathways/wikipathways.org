0c613b37a538d9a4d6cd14acc8a611a68c34bc06
mah <mah@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2013-04-20 20:55:21 +0000

    avoid error by using $wgRequest

--- wpi/Javascript.php
+++ wpi/Javascript.php
@@ -5,17 +5,17 @@
 $wgHooks['OutputPageParserOutput'][] = 'wpiAddJavascript';
 
 function wpiAddJavascript(&$out, $parseroutput) {
-	global $wgJsMimeType, $wpiJavascriptSnippets, $wpiJavascriptSources, $jsRequireJQuery, $jsJQuery;
-	
+	global $wgJsMimeType, $wpiJavascriptSnippets, $wpiJavascriptSources, $jsRequireJQuery, $jsJQuery, $wgRequest;
+
 	//First add JQuery if required
 	if($jsRequireJQuery) {
 		$out->addScript("<script src=\"{$jsJQuery}\" type=\"{$wgJsMimeType}\"></script>\n");
 	}
-	
+
 	//Array containing javascript source files to add
 	if(!isset($wpiJavascriptSources)) $wpiJavascriptSources = array();
 	$wpiJavascriptSources = array_unique($wpiJavascriptSources);
-	
+
 	//Array containing javascript snippets to add
 	if(!isset($wpiJavascriptSnippets)) $wpiJavascriptSnippets = array();
 	$wpiJavascriptSnippets = array_unique($wpiJavascriptSnippets);
@@ -26,11 +26,11 @@ function wpiAddJavascript(&$out, $parseroutput) {
 	foreach($wpiJavascriptSources as $src) {
 		$out->addScript("<script src=\"{$src}\" type=\"{$wgJsMimeType}\"></script>\n");
 	}
-	
+
 	//Add firebug lite console if requested in GET
-	$fb = $_GET['firebug'];
+	$fb = $wgRequest->getval('firebug');
 	if($fb) {
-		$out->addScript("<script src=\"http://getfirebug.com/releases/lite/1.2/firebug-lite-compressed.js\" type=\"{$wgJsMimeType}\"></script>\n");	
+		$out->addScript("<script src=\"http://getfirebug.com/releases/lite/1.2/firebug-lite-compressed.js\" type=\"{$wgJsMimeType}\"></script>\n");
 	}
 	return true;
 }

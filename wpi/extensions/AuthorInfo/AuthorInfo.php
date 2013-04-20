c67d2ec6dcacda02b3fb4908180d57e338b35351
mah <mah@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2013-04-20 22:12:58 +0000

    fix E_STRICT

--- wpi/extensions/AuthorInfo/AuthorInfo.php
+++ wpi/extensions/AuthorInfo/AuthorInfo.php
@@ -6,18 +6,22 @@ $wgAjaxExportList[] = "jsGetAuthors";
 $wfAuthorInfoPath = WPI_URL . "/extensions/AuthorInfo";
 
 function wfAuthorInfo() {
-    global $wgParser;
-    $wgParser->setHook( "AuthorInfo", "renderAuthorInfo" );
+	global $wgParser;
+	$wgParser->setHook( "AuthorInfo", "renderAuthorInfo" );
 }
 
 function renderAuthorInfo($input, $argv, &$parser) {
 	global $wfAuthorInfoPath;
 	$parser->disableCache();
-	
-	$limit = htmlentities($argv["limit"]);
-	if(!$limit) $limit = 0;
-	$bots = htmlentities($argv["bots"]);
-	if(!$bots) $bots = false;
+
+	if( isset( $argv["limit"] ) )
+		$limit = htmlentities( $argv["limit"] );
+	else
+		$limit = 0;
+	if( isset( $argv["bots"] ) )
+		$bots = htmlentities( $argv["bots"] );
+	else
+		$bots = false;
 
 	//Add CSS
 	//Hack to add a css that's not in the skins directory
@@ -26,7 +30,7 @@ function renderAuthorInfo($input, $argv, &$parser) {
 	$wgStylePath = $wfAuthorInfoPath;
 	$wgOut->addStyle("AuthorInfo.css");
 	$wgStylePath = $oldStylePath;
-	
+
 	$html = "<script type=\"text/javascript\" src=\"$wfAuthorInfoPath/AuthorInfo.js\"></script>\n";
 	$id = $parser->getTitle()->getArticleId();
 	$html .= "<div id='authorInfoContainer'></div><script type=\"text/javascript\">" .
@@ -56,28 +60,28 @@ class AuthorInfoList {
 	private $title;
 	private $limit;
 	private $showBots;
-	
+
 	private $authors;
-	
+
 	public function __construct($title, $limit = '', $showBots = false) {
 		$this->title = $title;
 		if($limit) $this->limit = $limit + 1;
 		$this->showBots = $showBots;
 		$this->load();
 	}
-	
+
 	private function load() {
 		$dbr = wfGetDB( DB_SLAVE );
 		$limit = '';
 		if($this->limit) {
 			$limit = "LIMIT 0, {$this->limit}";
 		}
-		
+
 		//Get users for page
 		$page_id = $this->title->getArticleId();
 		$query = "SELECT DISTINCT(rev_user) FROM revision WHERE " .
 			"rev_page = {$page_id} $limit";
-			
+
 		$res = $dbr->query($query);
 		$this->authors = array();
 		while($row = $dbr->fetchObject( $res )) {
@@ -87,12 +91,12 @@ class AuthorInfoList {
 				$this->authors[] = new AuthorInfo($user, $this->title);
 			}
 		}
-		
+
 		//Sort the authors by editCount
 		usort($this->authors, "AuthorInfo::compareByEdits");
 		$dbr->freeResult( $res );
 	}
-	
+
 	/**
 	 * Render the author list.
 	 * @return A HTML snipped containing the author list
@@ -104,7 +108,7 @@ class AuthorInfoList {
 		}
 		return substr($html, 0, -2);
 	}
-	
+
 	/**
 	 * Get an XML document containing the author info
 	 */
@@ -112,7 +116,7 @@ class AuthorInfoList {
 		$doc = new DOMDocument();
 		$root = $doc->createElement("AuthorList");
 		$doc->appendChild($root);
-		
+
 		foreach($this->authors as $a) {
 			$a->addXml($doc, $root);
 		}
@@ -124,17 +128,17 @@ class AuthorInfo {
 	private $title;
 	private $user;
 	private $editCount;
-	
+
 	public function __construct($user, $title) {
 		$this->title = $title;
 		$this->user = $user;
 		$this->load();
 	}
-	
+
 	public function getEditCount() {
 		return $this->editCount;
 	}
-	
+
 	private function load() {
 		$dbr = wfGetDB( DB_SLAVE );
 		$query = "SELECT COUNT(rev_user) AS editCount FROM revision " .
@@ -145,10 +149,10 @@ class AuthorInfo {
 		$this->editCount = $row->editCount;
 		$dbr->freeResult( $res );
 	}
-	
+
 	public function getDisplayName() {
 		$name = $this->user->getRealName();
-		
+
 		//Filter out email addresses
 		if(preg_match("/^[-_a-z0-9\'+*$^&%=~!?{}]++(?:\.[-_a-z0-9\'+*$^&%=~!?{}]+)*+@(?:(?![-.])[-a-z0-9.]+(?<![-.])\.[a-z]{2,6}|\d{1,3}(?:\.\d{1,3}){3})(?::\d++)?$/iD", $name)) {
 			$name = ''; //use username instead
@@ -156,14 +160,14 @@ class AuthorInfo {
 		if(!$name) $name = $this->user->getName();
 		return $name;
 	}
-	
+
 	private function getAuthorLink() {
 		global $wgScriptPath;
 		$title = Title::newFromText('User:' . $this->user->getTitleKey());
 		$href = $title->getFullUrl();
 		return $href;
 	}
-	
+
 	/**
 	 * Creates the HTML code to display a single
 	 * author
@@ -175,7 +179,7 @@ class AuthorInfo {
 			htmlspecialchars($name) . "</A>";
 		return $link;
 	}
-	
+
 	/**
 	 * Add an XML node for this author to the
 	 * given node.
@@ -187,7 +191,7 @@ class AuthorInfo {
 		$e->setAttribute("Url", $this->getAuthorLink());
 		$node->appendChild($e);
 	}
-	
+
 	public static function compareByEdits($a1, $a2) {
 		$c = $a2->getEditCount() - $a1->getEditCount();
 		if($c == 0) { //If equal edits, compare by realname

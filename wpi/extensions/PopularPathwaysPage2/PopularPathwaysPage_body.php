7e22aa5fc6de8e21106cbc74e768bca9a73a73f5
mah <mah@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2013-04-22 23:57:21 +0000

    w/s and loadMessages() is a static method

--- wpi/extensions/PopularPathwaysPage2/PopularPathwaysPage_body.php
+++ wpi/extensions/PopularPathwaysPage2/PopularPathwaysPage_body.php
@@ -2,36 +2,36 @@
 require_once("QueryPage.php");
 
 class PopularPathwaysPage extends SpecialPage
-{		
-        function PopularPathwaysPage() {
-                SpecialPage::SpecialPage("PopularPathwaysPage");
-                self::loadMessages();
-        }
+{
+		function PopularPathwaysPage() {
+				SpecialPage::SpecialPage("PopularPathwaysPage");
+				self::loadMessages();
+		}
 
-        function execute( $par ) {
-                global $wgRequest, $wgOut;
-                
-                $this->setHeaders();
+		function execute( $par ) {
+				global $wgRequest, $wgOut;
 
-                list( $limit, $offset ) = wfCheckLimits();
+				$this->setHeaders();
+
+				list( $limit, $offset ) = wfCheckLimits();
 
 				$ppp = new PPQueryPage();
 
 				return $ppp->doQuery( $offset, $limit );
-        }
+		}
 
-        function loadMessages() {
-                static $messagesLoaded = false;
-                global $wgMessageCache;
-                if ( $messagesLoaded ) return true;
-                $messagesLoaded = true;
+		static function loadMessages() {
+				static $messagesLoaded = false;
+				global $wgMessageCache;
+				if ( $messagesLoaded ) return true;
+				$messagesLoaded = true;
 
-                require( dirname( __FILE__ ) . '/PopularPathwaysPage.i18n.php' );
-                foreach ( $allMessages as $lang => $langMessages ) {
-                        $wgMessageCache->addMessages( $langMessages, $lang );
-                }
-                return true;
-        }
+				require( dirname( __FILE__ ) . '/PopularPathwaysPage.i18n.php' );
+				foreach ( $allMessages as $lang => $langMessages ) {
+						$wgMessageCache->addMessages( $langMessages, $lang );
+				}
+				return true;
+		}
 }
 
 class PPQueryPage extends QueryPage {
@@ -52,29 +52,28 @@ class PPQueryPage extends QueryPage {
 
 		return
 			"SELECT 'Popularpages' as type,
-			        page_namespace as namespace,
-			        page_title as title,
+					page_namespace as namespace,
+					page_title as title,
 				page_id as id,
-			        page_counter as value
+					page_counter as value
 			FROM $page
-			WHERE page_namespace=".NS_PATHWAY." 
+			WHERE page_namespace=".NS_PATHWAY."
 			AND page_is_redirect=0";
 	}
 
 	function formatResult( $skin, $result ) {
 		global $wgLang, $wgContLang;
 		$taggedIds = CurationTag::getPagesForTag('Curation:Tutorial');
-		if (in_array($result->id, $taggedIds)){ 
+		if (in_array($result->id, $taggedIds)){
 			return null;
 		}
 		$pathway = Pathway::newFromTitle($result->title);
 		if(!$pathway->isReadable()) return null; //Skip private pathways
 		$title = Title::makeTitle( $result->namespace, $pathway->getSpecies().":".$pathway->getName() );
-                $id = Title::makeTitle( $result->namespace, $result->title );
+				$id = Title::makeTitle( $result->namespace, $result->title );
 		$link = $skin->makeKnownLinkObj( $id, htmlspecialchars( $wgContLang->convert( $title->getBaseText() ) ) );
 		$nv = wfMsgExt( 'nviews', array( 'parsemag', 'escape'),
 			$wgLang->formatNum( $result->value ) );
 		return wfSpecialList($link, $nv);
 	}
 }
-?>

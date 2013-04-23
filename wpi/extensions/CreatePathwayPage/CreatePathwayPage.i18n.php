a3e036d523f39d8156e7da856d3f479687c26822
mah <mah@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2013-04-23 00:31:02 +0000

    w/s and such

--- wpi/extensions/CreatePathwayPage/CreatePathwayPage.i18n.php
+++ wpi/extensions/CreatePathwayPage/CreatePathwayPage.i18n.php
@@ -3,9 +3,8 @@ global $wgUser;
 $skin = $wgUser->getSkin();
 $link = $skin->makeLinkObj(Title::makeTitle( NS_HELP, Private_Pathways ), ' (how does that work?)');
 $allMessages = array(
-        'en' => array( 
-                'createpathwaypage' => 'Create new pathway',
-                'create_private' => "Make pathway private  $link",
-        )
+		'en' => array(
+				'createpathwaypage' => 'Create new pathway',
+				'create_private' => "Make pathway private  $link",
+		)
 );
-?>

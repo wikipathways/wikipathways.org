3973a3262c92cc07d2f4d3ed36f2c6e672a9aff8
mah <mah@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2013-04-23 00:45:14 +0000

    Must quote strings

--- wpi/extensions/CreatePathwayPage/CreatePathwayPage.i18n.php
+++ wpi/extensions/CreatePathwayPage/CreatePathwayPage.i18n.php
@@ -1,7 +1,7 @@
 <?php
 global $wgUser;
 $skin = $wgUser->getSkin();
-$link = $skin->makeLinkObj(Title::makeTitle( NS_HELP, Private_Pathways ), ' (how does that work?)');
+$link = $skin->makeLinkObj(Title::makeTitle( NS_HELP, 'Private_Pathways' ), ' (how does that work?)');
 $allMessages = array(
 		'en' => array(
 				'createpathwaypage' => 'Create new pathway',

39665e91ecf4f0dbbc27a5649f44c1c3d954e6ae
mah <mah@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2013-04-30 20:46:07 +0000

    JS updates

--- wpi/extensions/PageEditor/PageEditor.js
+++ wpi/extensions/PageEditor/PageEditor.js
@@ -9,21 +9,21 @@ function PageEditor(targetId, type, content, pwId) {
 PageEditor.images = {
 	edit:  '/skins/common/images/edit.png',
 	ok:  '/skins/common/images/apply.png',
-	cancel:  '/skins/common/images/cancel.png',
-}
+	cancel:  '/skins/common/images/cancel.png'
+};
 
 PageEditor.prototype.addEditButton = function() {
 	var that = this;
 	this.$edit = $('<img class="pageEditBtn" src="' + PageEditor.images.edit + '" />').attr('title', 'Edit ' + this.type);
 	this.$target.before(this.$edit);
 	this.$edit.click(function() { that.startEditor(); });
-}
+};
 
 PageEditor.prototype.startEditor = function() {
 	var that = this;
-	
+
 	this.$container = $('<div>');
-	
+
 	if(this.type == "category") {
 		this.$editor = $('<div>');
 		var categories = $.parseJSON(this.content);
@@ -44,47 +44,47 @@ PageEditor.prototype.startEditor = function() {
 			});
 	} else { //Text area by default
 		this.$editor = $('<textarea class="pageEditText">' + this.content + '</textarea>');
-	}	
-	
+	}
+
 	this.$container.append(this.$editor);
-	
+
 	this.$target.replaceWith(this.$container);
-	
+
 	//Replace edit button with save/cancel
 	$ok = $('<img src="' + PageEditor.images.ok + '" />');
 	$cancel = $('<img src="' + PageEditor.images.cancel + '" />');
 	this.$okcancel = $('<div/>');
 	this.$okcancel.append($ok);
 	this.$okcancel.append($cancel);
-	
+
 	$ok.click(function() { that.save(); });
 	$cancel.click(function() { that.cancelEditor(); });
-	
+
 	this.$edit.remove();
 	this.$container.append(this.$okcancel);
-}
+};
 
 PageEditor.prototype.cancelEditor = function() {
 	this.$container.replaceWith(this.$target);
 	this.$okcancel.remove();
 	this.addEditButton();
-}
+};
 
 PageEditor.prototype.save = function() {
 	var that = this;
-	
+
 	//Block edit controls
 	this.$block = $('<div>').addClass('editblock');
 	this.$block.width(this.$container.width() + 'px');
 	this.$block.height(this.$container.height() + 'px');
 	this.$container.after(this.$block);
-	
+
 	this.$block.position({
 		my: "left top",
 		at: "left top",
 		of: this.$container
 	});
-	
+
 	var val = "";
 	if(this.type == "category") {
 		val = '{';
@@ -97,14 +97,14 @@ PageEditor.prototype.save = function() {
 	} else {
 		val = this.$editor.val();
 	}
-	
+
 	//Perform save
 	sajax_do_call(
-		"PageEditor::save", 
-		[this.pwId, this.type, val], 
+		"PageEditor::save",
+		[this.pwId, this.type, val],
 		function(xhr) { that.afterSave(xhr); }
 	);
-}
+};
 
 PageEditor.prototype.afterSave = function(xhr) {
 	if(this.checkResponse(xhr)) {
@@ -112,7 +112,7 @@ PageEditor.prototype.afterSave = function(xhr) {
 	} else {
 		this.$block.remove();
 	}
-}
+};
 
 PageEditor.prototype.checkResponse = function(xhr) {
 	if (xhr.readyState == 4){
@@ -120,8 +120,10 @@ PageEditor.prototype.checkResponse = function(xhr) {
 			return true;
 		} else {
 			window.alert("Unable to save: " + xhr.statusText);
+			return false;
 		}
 	} else {
 		window.alert("Unable to save: " + xhr.statusText);
+		return false;
 	}
-}
+};

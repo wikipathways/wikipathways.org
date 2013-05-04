07be376cfce04ad56b84a61af60aa3eeaa7bedcb
mah <mah@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2013-05-04 19:44:00 +0000

    * Declare $tried so we avoid undef warnings. * Only clear history once, second time is redundant.

--- wpi/extensions/PathwayOfTheDay.php
+++ wpi/extensions/PathwayOfTheDay.php
@@ -14,7 +14,7 @@ We need:
 Concerning MediaWiki:
 	- create a new SpecialPage: Special:PathwayOfTheDay
 	- create an extension that implements above in php
-	
+
 We need:
 	- to pick a random pathway everyday (from all articles in namespace pathway)
 	- remember this pathway and the day it was picked, store that in cache
@@ -28,22 +28,22 @@ $wgExtensionFunctions[] = 'wfPathwayOfTheDay';
 $wgHooks['LanguageGetMagic'][]  = 'wfPathwayOfTheDay_Magic';
 
 function wfPathwayOfTheDay() {
-        global $wgParser;
-        # Set a function hook associating the "example" magic word with our function
-        $wgParser->setFunctionHook( 'pathwayOfTheDay', 'getPathwayOfTheDay' );
+		global $wgParser;
+		# Set a function hook associating the "example" magic word with our function
+		$wgParser->setFunctionHook( 'pathwayOfTheDay', 'getPathwayOfTheDay' );
 }
 
 function wfPathwayOfTheDay_Magic( &$magicWords, $langCode ) {
-        # Add the magic word
-        # The first array element is case sensitive, in this case it is not case sensitive
-        # All remaining elements are synonyms for our parser function
-        $magicWords['pathwayOfTheDay'] = array( 0, 'pathwayOfTheDay' );
-        # unless we return true, other parser functions extensions won't get loaded.
-        return true;
+		# Add the magic word
+		# The first array element is case sensitive, in this case it is not case sensitive
+		# All remaining elements are synonyms for our parser function
+		$magicWords['pathwayOfTheDay'] = array( 0, 'pathwayOfTheDay' );
+		# unless we return true, other parser functions extensions won't get loaded.
+		return true;
 }
 
 function getPathwayOfTheDay( &$parser, $date, $listpage = 'FeaturedPathways', $isTag = false) {
-	$parser->disableCache();	
+	$parser->disableCache();
 	wfDebug("GETTING PATHWAY OF THE DAY for date: $date\n");
 	try {
 		if($isTag) {
@@ -62,12 +62,12 @@ function getPathwayOfTheDay( &$parser, $date, $listpage = 'FeaturedPathways', $i
 
 class TaggedPathway extends PathwayOfTheDay {
 	private $tag;
-	
+
 	function __construct($id, $date, $tag) {
 		$this->tag = $tag;
 		parent::__construct($id, $date);
 	}
-	
+
 	/**
 	Select a random pathway from all pathways
 	with the given tag
@@ -98,12 +98,12 @@ pathways from a limited collection, kept on the FeaturedPathway wiki page
 **/
 class FeaturedPathway extends PathwayOfTheDay {
 	private $listPage;
-	
+
 	function __construct($id, $date, $listPage) {
 		$this->listPage = $listPage;
 		parent::__construct($id, $date);
 	}
-	
+
 	/**
 	Select a random pathway from the list
 	on the wiki page FeaturedPathway
@@ -120,11 +120,11 @@ class FeaturedPathway extends PathwayOfTheDay {
 
 class PathwayOfTheDay {
 	private static $table = 'pathwayOfTheDay';
-	
+
 	var $todaysPw; //Todays pathway
 	var $today; //Day todaysPw was marked as today's
 	var $id; //Id to support multiple pathway of the day caches
-	
+
 	function __construct($id, $date) {
 		PathwayOfTheDay::setupDB(); //TODO: remove this after first use
 		$this->id = $id;
@@ -135,7 +135,7 @@ class PathwayOfTheDay {
 		}
 		$this->todaysPw = $this->fetchTodaysPathway();
 	}
-	
+
 	public function getWikiOutput() {
 		if($this->today == '{{{date}}}') {//Template variable not set, use dummy return values
 			$pw = "TemplatePathway";
@@ -150,7 +150,7 @@ class PathwayOfTheDay {
 		}
 		return "{{Template:TodaysPathway|pwName=$name|pwSpecies=$species|article=$article|image=$image|date=$date}}";
 	}
-	
+
 	private function fetchTodaysPathway() {
 		$dbr =& wfGetDB(DB_SLAVE);
 		$res = $dbr->select( PathwayOfTheDay::$table, array('pathway'), array('day' => $this->id . $this->today));
@@ -158,7 +158,7 @@ class PathwayOfTheDay {
 		$dbr->freeResult( $res );
 		return $row[0];
 	}
-	
+
 	//Get the pathway for today
 	public function todaysPathway() {
 		if(!$this->todaysPw) { //No pathway in history yet
@@ -178,7 +178,7 @@ class PathwayOfTheDay {
 		}
 		return $pathway;
 	}
-	
+
 	//Create and fill the tables
 	private static function setupDB() {
 		$tbl = PathwayOfTheDay::$table;
@@ -188,46 +188,45 @@ class PathwayOfTheDay {
 		#Index...doesn't work yet
 		/*$dbw->query( "IF NOT EXISTS (SELECT * from SYSINDEXES
 						WHERE id=object_id('$tbl') AND name='$ind_pw')
-						CREATE INDEX $ind_pw on $tbl(pathway)", 
+						CREATE INDEX $ind_pw on $tbl(pathway)",
 					DB_MASTER );
 		$dbw->query( "IF NOT EXISTS (SELECT * from SYSINDEXES
 						WHERE id=object_id('$tbl') AND name='$ind_day')
-						CREATE INDEX $ind_day on $tbl(pathway)", 
+						CREATE INDEX $ind_day on $tbl(pathway)",
 					DB_MASTER );
 					*/
 		wfDebug("\tDone!\n");
 	}
-	
+
 	//A brand new day, fetch new random patwhay that we haven't had before
 	private function brandNewDay() {
 		wfDebug("\tA brand new day....refreshing pathway of the day\n");
 		$this->findFreshPathway();
 	}
-	
+
 	private function findFreshPathway() {
 		wfDebug("\tSearching for fresh pathway\n");
 		$pw = $this->fetchRandomPathway();
 		wfDebug("\t\tPathway in cache: '$pw'\n");
+		$tried = 0;
 		while($this->hadBefore($pw)) {
-			//Keep on searching until we found one that we haven't had before 
-			$pw = $this->fetchRandomPathway();	
+			//Keep on searching until we found one that we haven't had before
+			$pw = $this->fetchRandomPathway();
 			wfDebug("\t\tTrying: '$pw'\n");
 			$tried++;
 			wfDebug("\t\t\t$tried attempt\n");
-			if($tried > 100) { 
+			if($tried > 100) {
 				wfDebug("\tTried too often, clearing history\n");
 				//However, if we tried too often, just pick a pathway and reset the pathway list
 				//TODO: 'too often' needs to be the number of pathways...
 				$this->clearHistory();
-				$this->clearHistory();
-				$tried = 0;
 			}
 		}
 		$this->todaysPw = $pw;
 		//We found  a new pathway, now update history
 		$this->updateHistory();
 	}
-	
+
 	private function hadBefore($pathway) {
 		wfDebug("\tDid we have $pathway before? ");
 		if(!$pathway) {
@@ -242,18 +241,18 @@ class PathwayOfTheDay {
 		wfDebug(" $had\n");
 		return $had;
 	}
-	
+
 	private function clearHistory() {
 		$dbw =& wfGetDB(DB_MASTER);
 		wfDebug("\tClearing history\n");
 		$dbw->query( "TRUNCATE TABLE " . PathwayOfTheDay::$table, DB_MASTER );
 	}
-		
+
 	private function updateHistory() {
 		$dbw =& wfGetDB(DB_MASTER);
 		$dbw->insert(PathwayOfTheDay::$table, array('pathway' => $this->todaysPw, 'day' => $this->id . $this->today));
 	}
-	
+
 	//Select a random pathway
 	protected function fetchRandomPathway() {
 		wfDebug("Fetching random pathway...\n");

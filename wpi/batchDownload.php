c82768d14bb6fc7144418a7e4120a6affe66ea57
mah <mah@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2013-05-04 22:33:21 +0000

    moar checking

--- wpi/batchDownload.php
+++ wpi/batchDownload.php
@@ -19,12 +19,12 @@ function wfBatchDownload() {
 //To be called directly
 if(realpath($_SERVER['SCRIPT_FILENAME']) == realpath(__FILE__)) {
 	wfDebug("PROCESSING BATCH DOWNLOAD\n");
-	$species = $_GET['species'];
-	$fileType = $_GET['fileType'];
-	$listPage = $_GET['listPage'];
-	$tag = $_GET['tag'];
-	$excludeTags = $_GET['tag_excl'];
-	$displayStats = $_GET['stats'];
+	$species      = isset( $_GET['species'] )  ? $_GET['species'] : "";
+	$fileType     = isset( $_GET['fileType'] ) ? $_GET['fileType'] : "";
+	$listPage     = isset( $_GET['listPage'] ) ? $_GET['listPage'] : "";
+	$tag          = isset( $_GET['tag'] )      ? $_GET['tag'] : "";
+	$excludeTags  = isset( $_GET['tag_excl'] ) ? $_GET['tag_excl'] : "";
+	$displayStats = isset( $_GET['stats'] )    ? $_GET['stats'] : "";
 
 	if($species) {
 		try {

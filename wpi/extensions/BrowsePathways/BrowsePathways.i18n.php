19afa403eca2a87c923ce11f658523714fef3aba
mah <mah@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2013-05-15 22:13:33 +0000

    what dummy forgot to close his parens?

--- wpi/extensions/BrowsePathways/BrowsePathways.i18n.php
+++ wpi/extensions/BrowsePathways/BrowsePathways.i18n.php
@@ -4,3 +4,4 @@ $messages['en'] = array(
     'browsepathways-selectspecies' => 'Display pathways from species:',
     'browsepathways-all-species' => 'All Species',
     'browsepathways-uncategorized-species' => 'Uncategorized'
+);

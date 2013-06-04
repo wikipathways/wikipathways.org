1cbd9cfd40599cc7bd46e90540224ac4511b2ece
mah <mah@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2013-06-04 00:19:21 +0000

    formatting and scoping

--- skins/wikipathways/wikipathways-custom.js
+++ skins/wikipathways/wikipathways-custom.js
@@ -8,43 +8,45 @@ function doToggle( elId, msg, expand, collapse ) {
     }
 }
 
-/* From http://css-tricks.com/equal-height-blocks-in-rows/ */
-var currentTallest = 0,
-     currentRowStart = 0,
-     rowDivs = new Array(),
-     $el,
-     topPosition = 0;
-$(document).ready( function() {
-    $('.browsePathways').each(function() {
-                                   $el = $(this);
+$(document).ready(
+    function() {
+        /* From http://css-tricks.com/equal-height-blocks-in-rows/ */
+        var currentTallest = 0,
+        currentRowStart = 0,
+        rowDivs = new Array(),
+        $el,
+        topPosition = 0;
+        $('.browsePathways').each(
+            function() {
+                $el = $(this);
 
-                                   topPostion = $el.position().top;
+                topPostion = $el.position().top;
 
-                                   if (currentRowStart != topPostion) {
+                if (currentRowStart != topPostion) {
 
-                                       // we just came to a new row.  Set all the heights on the completed row
-                                       for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
-                                           rowDivs[currentDiv].height(currentTallest);
-                                       }
+                    // we just came to a new row.  Set all the heights on the completed row
+                    for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                        rowDivs[currentDiv].height(currentTallest);
+                    }
 
-                                       // set the variables for the new row
-                                       rowDivs.length = 0; // empty the array
-                                       currentRowStart = topPostion;
-                                       currentTallest = $el.height();
-                                       rowDivs.push($el);
+                    // set the variables for the new row
+                    rowDivs.length = 0; // empty the array
+                    currentRowStart = topPostion;
+                    currentTallest = $el.height();
+                    rowDivs.push($el);
 
-                                   } else {
+                } else {
 
-                                       // another div on the current row.  Add it to the list and check if it's taller
-                                       rowDivs.push($el);
-                                       currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+                    // another div on the current row.  Add it to the list and check if it's taller
+                    rowDivs.push($el);
+                    currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
 
-                                   }
+                }
 
-                                   // do the last row
-                                   for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
-                                       rowDivs[currentDiv].height(currentTallest);
-                                   }
+                // do the last row
+                for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                    rowDivs[currentDiv].height(currentTallest);
+                }
 
-});
-});
+            });
+    });

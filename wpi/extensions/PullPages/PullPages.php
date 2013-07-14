3199d6c4bc88cceb9eafa884bf43549b5a914003
mah <mah@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2013-07-14 22:30:03 +0000

    add page puller to autoloader

--- wpi/extensions/PullPages/PullPages.php
+++ wpi/extensions/PullPages/PullPages.php
@@ -4,3 +4,4 @@ $pullSite = "http://test.wikipathways.org/index.php";
 $pullPages = "MediaWiki:PagesToPull";
 
 $wgAutoloadClasses['PullPages'] = dirname(__FILE__) . '/PullPages_class.php';
+$wgAutoloadClasses['PagePuller'] = dirname(__FILE__) . '/PagePuller.php';

c37d5afd5bf05ff7feba8b169298ff7d3d3df318
tina <tina@4f21837e-9f06-0410-ae49-bac5c3a7b9b6>
2013-10-04 09:02:34 +0000

    bug fix 1678 - link to private pathway help page.

--- wpi/extensions/CreatePathwayPage/CreatePathwayPage.i18n.php
+++ wpi/extensions/CreatePathwayPage/CreatePathwayPage.i18n.php
@@ -2,7 +2,7 @@
 $allMessages = array(
 		'en' => array(
 			'createpathwaypage' => 'Create new pathway',
-			'create_private' => "Make pathway private ([[Help::Private Pathway|how does this work?]])",
+			'create_private' => "Make pathway private ([[:Help:Private Pathways|how does this work?]])",
 			'wpi-createpage-not-logged-in' => "You are not logged in.",
 			'wpi-createpage-permission'    => "You do not have the necessary permissions."
 

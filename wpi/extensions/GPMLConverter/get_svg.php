10e4d59a40b1b511ce5297f088209db49357c69e
Anders Riutta <git@andersriutta.com>
2017-12-01 16:15:17 -0800

    Now successfully converting.

--- wpi/extensions/GPMLConverter/get_svg.php
+++ wpi/extensions/GPMLConverter/get_svg.php
@@ -4,9 +4,10 @@
 	 </head>
 	 <body>
 		 <?php
+		 $jq="/nix/var/nix/profiles/default/bin/jq";
 		 $gpml2pvjson="/nix/var/nix/profiles/default/bin/gpml2pvjson";
 		 $bridgedb="/nix/var/nix/profiles/default/bin/bridgedb";
-		 $jq="/nix/var/nix/profiles/default/bin/jq";
+		 $pvjs="/nix/var/nix/profiles/default/bin/pvjs";
 
 		 $gpml_file = '/var/www/dev.wikipathways.org/wpi/extensions/GPMLConverter/WP2864_79278.gpml';
 		 $pathway_identifier = 'WP2864';
@@ -25,7 +26,8 @@ cat "$gpml_file" | \
 	$jq --slurp 'reduce .[] as \$entity ({}; .[\$entity.id] = \$entity)' > \$entity_map_file;
 
 cat "\$entity_map_file" | \
-	$jq -rc --slurpfile original_json "\$original_json_file" --slurp '. as \$entity_map | ({pathway: \$original_json[0].pathway, entityMap: \$entity_map[0]})'
+	$jq -rc --slurpfile original_json "\$original_json_file" --slurp '. as \$entity_map | ({pathway: \$original_json[0].pathway, entityMap: \$entity_map[0]})' | \
+	$pvjs json2svg -s false;
 TEXT;
 
 		$unifiedJson = shell_exec($cmd);

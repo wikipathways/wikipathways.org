5ba638a3380571b77ac5545daaf6ce45a78e3a2a
Anders Riutta <git@andersriutta.com>
2017-12-11 23:22:30 -0800

    Undo http -> https for dev.

--- wpi/globals.php
+++ wpi/globals.php
@@ -21,7 +21,7 @@ $host = isset( $_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : "wikipathways.or
 $wpiScriptPath = realpath(dirname(__FILE__));
 $wpiScript = "$wpiScriptPath/$wpiScriptFile"; 
 $wpiTmpPath = "$wpiScriptPath/$wpiTmpName";
-$siteURL = "https://$host/$wgScriptPath";
+$siteURL = "http://$host/$wgScriptPath";
 $wpiURL = "$siteURL/$wpiPathName";
 $wpiCachePath = "$wpiScriptPath/$wpiCacheName";
 

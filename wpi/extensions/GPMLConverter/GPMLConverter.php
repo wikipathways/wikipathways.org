5cf3f3d1bfa7fbf4679096b91a6dc17841e3ec77
Anders Riutta <git@andersriutta.com>
2018-01-09 10:51:32 -0800

    Re-enable xref unification.

--- wpi/extensions/GPMLConverter/GPMLConverter.php
+++ wpi/extensions/GPMLConverter/GPMLConverter.php
@@ -103,14 +103,6 @@ TEXT;
 		//TODO: this timeout should be removed when we get async cacheing working
 		$streamGpml2Pvjson = create_stream("$toPvjsonCmd", array("timeout" => 4));
 		$rawPvjsonString = $streamGpml2Pvjson($gpml, true);
-		return $rawPvjsonString;
-
-		// TODO we disabled all unification, because it was overloading the server. We need to do caching or something.
-
-		//Skip bridgedb unification unless view=widget (i.e., where the unification is useful)
-		$view = isset($_GET["view"]) ? $_GET["view"] : "normal";
-		if($view != 'widget')
-			return $rawPvjsonString;
 
 ## TODO the enrich method from bridgedbjs is extremely slow when this was
 ## installed via Nix, but it may have been faster when installed via NPM.

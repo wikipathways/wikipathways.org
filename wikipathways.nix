9ee87e5787a689a4ef32d53d7d1496c1f65d00eb
Anders Riutta <git@andersriutta.com>
2018-03-30 17:57:59 -0700

    Add jenkins and virtuoso

--- wikipathways.nix
+++ wikipathways.nix
@@ -1,12 +1,15 @@
 with import <nixpkgs> {};
 let
+  # I don't know how this should be done. This is just a rough start.
+  # see https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/services/web-servers/apache-httpd/mediawiki.nix
+  apache = pkgs.apacheHttpd {extraModules = "mediawiki-1.29.1"}
   GPMLConverter = import ./wpi/extensions/GPMLConverter/default.nix;
   PathwayFinder = import ./wpi/extensions/PathwayFinder/default.nix;
 in [
-  pkgs.apacheHttpd
-  # see https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/services/web-servers/apache-httpd/mediawiki.nix
-  pkgs.mediawiki-1.29.1
+  apache
   pkgs.solr
+  pkgs.virtuoso7
+  pkgs.jenkins
   GPMLConverter
   PathwayFinder
 ]

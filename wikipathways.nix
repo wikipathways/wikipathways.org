8cc0cbea7797113b2d6747f3bb2f56be2c4bb455
Anders Riutta <git@andersriutta.com>
2018-03-30 17:59:03 -0700

    nix syntax

--- wikipathways.nix
+++ wikipathways.nix
@@ -2,7 +2,7 @@ with import <nixpkgs> {};
 let
   # I don't know how this should be done. This is just a rough start.
   # see https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/services/web-servers/apache-httpd/mediawiki.nix
-  apache = pkgs.apacheHttpd {extraModules = "mediawiki-1.29.1"}
+  apache = pkgs.apacheHttpd {extraModules = "mediawiki-1.29.1";}
   GPMLConverter = import ./wpi/extensions/GPMLConverter/default.nix;
   PathwayFinder = import ./wpi/extensions/PathwayFinder/default.nix;
 in [
